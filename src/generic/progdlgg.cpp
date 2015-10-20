Bricsys/wxWidgets

commit 5e605217a58551ea680c57c396b7888c0739a060

Fix invalid use of wxSizer flags in samples and utils.
Remove alignment flags that result in asserts because of being used in
combinations that don't make sense.

Also use wxSizerFlags in the lines that were changed.

src/generic/progdlgg.cpp
@@ -269,33 +269,35 @@ bool wxGenericProgressDialog::Create( const wxString& title,
 
     wxBoxSizer *buttonSizer = new wxBoxSizer(wxHORIZONTAL);
 
-    // Windows dialogs usually have buttons in the lower right corner
-    const int sizerFlags =
+    const int borderFlags =
 #if defined(__WXMSW__) || defined(__WXOSX__)
-                           wxALIGN_RIGHT | wxALL
-#else // !MSW
-                           wxALIGN_CENTER_HORIZONTAL | wxBOTTOM | wxTOP
-#endif // MSW/!MSW
-                           ;
+        wxALL
+#else
+        wxBOTTOM | wxTOP
+#endif
+        ;
+
+    const wxSizerFlags sizerFlags
+        = wxSizerFlags().Border(borderFlags, LAYOUT_MARGIN);
 
     if ( HasPDFlag(wxPD_CAN_SKIP) )
     {
         m_btnSkip = new wxButton(this, wxID_SKIP, _("&Skip"));
 
-        buttonSizer->Add(m_btnSkip, 0, sizerFlags, LAYOUT_MARGIN);
+        buttonSizer->Add(m_btnSkip, sizerFlags);
     }
 
     if ( HasPDFlag(wxPD_CAN_ABORT) )
     {
         m_btnAbort = new wxButton(this, wxID_CANCEL);
 
-        buttonSizer->Add(m_btnAbort, 0, sizerFlags, LAYOUT_MARGIN);
+        buttonSizer->Add(m_btnAbort, sizerFlags);
     }
 
     if ( !HasPDFlag(wxPD_CAN_SKIP | wxPD_CAN_ABORT) )
         buttonSizer->AddSpacer(LAYOUT_MARGIN);
 
-    sizerTop->Add(buttonSizer, 0, sizerFlags, LAYOUT_MARGIN );
+    sizerTop->Add(buttonSizer, sizerFlags);
 #endif // __SMARTPHONE__/!__SMARTPHONE__
 
     SetSizerAndFit(sizerTop);

samples/combo/combo.cpp
@@ -981,8 +981,8 @@ void MyFrame::OnShowComparison( wxCommandEvent& WXUNUSED(event) )
                                       wxVERTICAL);
 
     groupSizer->Add( new wxStaticText(dlg, wxID_ANY,
-                     wxT("Writable, with margins, sorted:")), 0,
-                     wxALIGN_CENTER_VERTICAL|wxRIGHT|wxEXPAND, border );
+                     wxT("Writable, with margins, sorted:")),
+                     wxSizerFlags().Expand().Border(wxRIGHT, border) );
 
     odc = new wxOwnerDrawnComboBox(dlg,wxID_ANY,wxEmptyString,
                                    wxDefaultPosition, wxDefaultSize,
@@ -994,14 +994,14 @@ void MyFrame::OnShowComparison( wxCommandEvent& WXUNUSED(event) )
 
     odc->SetValue(wxT("Dot Dash"));
     odc->SetMargins(15, 10);
-    groupSizer->Add( odc, 0, wxALIGN_CENTER_VERTICAL|wxALL, border );
+    groupSizer->Add( odc, wxSizerFlags().Border(wxALL, border) );
     groupSizer->AddStretchSpacer();
 
     //
     // Readonly ODComboBox
     groupSizer->Add( new wxStaticText(dlg, wxID_ANY,
-                     wxT("Read-only, big font:")), 0,
-                     wxALIGN_CENTER_VERTICAL|wxRIGHT, border );
+                     wxT("Read-only, big font:")),
+                     wxSizerFlags().Border(wxRIGHT, border) );
 
     odc = new wxOwnerDrawnComboBox(dlg,wxID_ANY,wxEmptyString,
                                    wxDefaultPosition, wxDefaultSize,
@@ -1018,8 +1018,8 @@ void MyFrame::OnShowComparison( wxCommandEvent& WXUNUSED(event) )
 
     //
     // Disabled ODComboBox
-    groupSizer->Add( new wxStaticText(dlg,wxID_ANY,wxT("Disabled:")), 0,
-                   wxALIGN_CENTER_VERTICAL|wxRIGHT, border );
+    groupSizer->Add( new wxStaticText(dlg,wxID_ANY,wxT("Disabled:")),
+                   wxSizerFlags().Border(wxRIGHT, border) );
 
     odc = new wxOwnerDrawnComboBox(dlg,wxID_ANY,wxEmptyString,
                                      wxDefaultPosition, wxDefaultSize,
@@ -1030,7 +1030,7 @@ void MyFrame::OnShowComparison( wxCommandEvent& WXUNUSED(event) )
     odc->SetValue(wxT("Dot Dash"));
     odc->Enable(false);
 
-    groupSizer->Add( odc, 3, wxALIGN_CENTER_VERTICAL|wxEXPAND|wxALL, border );
+    groupSizer->Add( odc, wxSizerFlags(3).Expand().Border(wxALL, border) );
 
     rowSizer->Add( groupSizer, 1, wxEXPAND|wxALL, border );
 
@@ -1042,8 +1042,8 @@ void MyFrame::OnShowComparison( wxCommandEvent& WXUNUSED(event) )
     // wxComboBox
     //
     groupSizer->Add( new wxStaticText(dlg,wxID_ANY,
-                     wxT("Writable, with margins, sorted:")), 0,
-                     wxALIGN_CENTER_VERTICAL|wxRIGHT|wxEXPAND, border );
+                     wxT("Writable, with margins, sorted:")),
+                     wxSizerFlags().Expand().Border(wxRIGHT, border) );
 
     cb = new wxComboBox(dlg,wxID_ANY,wxEmptyString,
                         wxDefaultPosition, wxDefaultSize,
@@ -1055,14 +1055,14 @@ void MyFrame::OnShowComparison( wxCommandEvent& WXUNUSED(event) )
 
     cb->SetValue(wxT("Dot Dash"));
     cb->SetMargins(15, 10);
-    groupSizer->Add( cb, 0, wxALIGN_CENTER_VERTICAL|wxALL, border );
+    groupSizer->Add( cb, wxSizerFlags().Border(wxALL, border) );
     groupSizer->AddStretchSpacer();
 
     //
     // Readonly wxComboBox
     groupSizer->Add( new wxStaticText(dlg, wxID_ANY,
-                     wxT("Read-only, big font:")), 0,
-                     wxALIGN_CENTER_VERTICAL|wxRIGHT, border );
+                     wxT("Read-only, big font:")),
+                     wxSizerFlags().Border(wxRIGHT, border) );
 
     cb = new wxComboBox(dlg,wxID_ANY,wxEmptyString,
                         wxDefaultPosition, wxDefaultSize,
@@ -1078,8 +1078,8 @@ void MyFrame::OnShowComparison( wxCommandEvent& WXUNUSED(event) )
 
     //
     // Disabled wxComboBox
-    groupSizer->Add( new wxStaticText(dlg,wxID_ANY,wxT("Disabled:")), 0,
-                   wxALIGN_CENTER_VERTICAL|wxRIGHT, border );
+    groupSizer->Add( new wxStaticText(dlg,wxID_ANY,wxT("Disabled:")),
+                   wxSizerFlags().Border(wxRIGHT, border) );
 
     cb = new wxComboBox(dlg,wxID_ANY,wxEmptyString,
                         wxDefaultPosition, wxDefaultSize,
@@ -1090,7 +1090,7 @@ void MyFrame::OnShowComparison( wxCommandEvent& WXUNUSED(event) )
     cb->SetValue(wxT("Dot Dash"));
     cb->Enable(false);
 
-    groupSizer->Add( cb, 3, wxALIGN_CENTER_VERTICAL|wxEXPAND|wxALL, border );
+    groupSizer->Add( cb, wxSizerFlags(3).Expand().Border(wxALL, border) );
 
     rowSizer->Add( groupSizer, 1, wxEXPAND|wxALL, border );
 

samples/dialogs/dialogs.cpp
@@ -3073,7 +3073,7 @@ wxPanel* SettingsDialog::CreateGeneralSettingsPage(wxWindow* parent)
     itemSizer8->Add(checkBox6, 0, wxALL|wxALIGN_CENTER_VERTICAL, 5);
     item0->Add(itemSizer8, 0, wxGROW|wxALL, 0);
 
-    topSizer->Add( item0, 1, wxGROW|wxALIGN_CENTRE|wxALL, 5 );
+    topSizer->Add( item0, wxSizerFlags(1).Expand().Border(wxALL, 5) );
 
     panel->SetSizerAndFit(topSizer);
 
@@ -3130,7 +3130,7 @@ wxPanel* SettingsDialog::CreateAestheticSettingsPage(wxWindow* parent)
     item0->Add(itemSizer5, 0, wxGROW|wxLEFT|wxRIGHT, 5);
 #endif
 
-    topSizer->Add( item0, 1, wxGROW|wxALIGN_CENTRE|wxALL, 5 );
+    topSizer->Add( item0, wxSizerFlags(1).Expand().Border(wxALL, 5) );
     topSizer->AddSpacer(5);
 
     panel->SetSizerAndFit(topSizer);
@@ -3211,7 +3211,7 @@ bool TestMessageBoxDialog::Create()
         sizerBtns->Add(m_buttons[n], wxSizerFlags().Centre().Left());
 
         m_labels[n] = new wxTextCtrl(this, wxID_ANY);
-        sizerBtns->Add(m_labels[n], wxSizerFlags().Centre().Expand());
+        sizerBtns->Add(m_labels[n], wxSizerFlags().Expand());
 
         m_labels[n]->Connect(wxEVT_UPDATE_UI,
                              wxUpdateUIEventHandler(

samples/ipc/client.cpp
@@ -189,15 +189,15 @@ MyFrame::MyFrame(wxFrame *frame, const wxString& title)
 
     item1->Add( item2, 1, wxALIGN_CENTER|wxALL, 5 );
 
-    item0->Add( item1, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5 );
+    item0->Add( item1, wxSizerFlags().Expand().Border(wxALL, 5) );
 
     wxStaticBox *item14 = new wxStaticBox( this, -1, wxT("Client log") );
     wxStaticBoxSizer *item13 = new wxStaticBoxSizer( item14, wxVERTICAL );
 
     wxTextCtrl *item15 = new wxTextCtrl( this, ID_LOG, wxEmptyString, wxDefaultPosition, wxSize(500,140), wxTE_MULTILINE );
-    item13->Add( item15, 1, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5 );
+    item13->Add( item15, wxSizerFlags(1).Expand().Border(wxALL, 5) );
 
-    item0->Add( item13, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5 );
+    item0->Add( item13, wxSizerFlags(0).Expand().Border(wxALL, 5) );
 
     this->SetSizer( item0 );
     item0->SetSizeHints( this );

samples/ipc/server.cpp
@@ -121,7 +121,7 @@ MyFrame::MyFrame(wxFrame *frame, const wxString& title)
     sizerCmds->Add(btn, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5);
     sizerCmds->AddSpacer(20);
 
-    sizerMain->Add(sizerCmds, 0, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5);
+    sizerMain->Add(sizerCmds, wxSizerFlags().Expand().Border(wxALL, 5));
 
     wxStaticBoxSizer * const
         sizerLog = new wxStaticBoxSizer(wxVERTICAL, panel, "Server &log");
@@ -134,9 +134,9 @@ MyFrame::MyFrame(wxFrame *frame, const wxString& title)
                                     wxDefaultPosition, wxSize(500, 140),
                                     wxTE_MULTILINE
                                  );
-    sizerLog->Add(textLog, 1, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5);
+    sizerLog->Add(textLog, wxSizerFlags(1).Expand().Border(wxALL, 5));
 
-    sizerMain->Add(sizerLog, 1, wxGROW|wxALIGN_CENTER_VERTICAL|wxALL, 5);
+    sizerMain->Add(sizerLog, wxSizerFlags(1).Expand().Border(wxALL, 5));
 
     panel->SetSizer(sizerMain);
     sizerMain->SetSizeHints(panel);

samples/propgrid/tests.cpp
@@ -173,7 +173,7 @@ void FormMain::OnDumpList( wxCommandEvent& WXUNUSED(event) )
         wxALIGN_CENTRE_HORIZONTAL|wxALIGN_CENTRE_VERTICAL|wxBOTTOM|wxLEFT|wxRIGHT;
     rowsizer->Add( new wxButton(dlg,wxID_OK,wxT("Ok")),
         0, butSzFlags, spacing );
-    topsizer->Add( rowsizer, 0, wxALIGN_RIGHT|wxALIGN_CENTRE_VERTICAL, 0 );
+    topsizer->Add( rowsizer, wxSizerFlags().Right() );
 
     dlg->SetSizer( topsizer );
     topsizer->SetSizeHints( dlg );
@@ -354,7 +354,7 @@ bool FormMain::RunTests( bool fullTest, bool interactive )
         wxALIGN_CENTRE_HORIZONTAL|wxALIGN_CENTRE_VERTICAL|wxBOTTOM|wxLEFT|wxRIGHT;
     rowsizer->Add( new wxButton(dlg,wxID_OK,wxT("Ok")),
         0, butSzFlags, spacing );
-    topsizer->Add( rowsizer, 0, wxALIGN_RIGHT|wxALIGN_CENTRE_VERTICAL, 0 );
+    topsizer->Add( rowsizer, wxSizerFlags().Right() );
 
     dlg->SetSizer( topsizer );
     topsizer->SetSizeHints( dlg );

samples/widgets/bmpcombobox.cpp
@@ -322,7 +322,7 @@ void BitmapComboBoxWidgetsPage::CreateContent()
     wxButton *btn = new wxButton(this, BitmapComboBoxPage_Reset, wxT("&Reset"));
     sizerStyle->Add(btn, 0, wxALIGN_CENTRE_HORIZONTAL | wxALL, 3);
 
-    sizerLeft->Add(sizerStyle, 0, wxGROW | wxALIGN_CENTRE_HORIZONTAL);
+    sizerLeft->Add(sizerStyle, wxSizerFlags().Expand());
     sizerLeft->Add(m_radioKind, 0, wxGROW | wxALL, 5);
 
     // left pane - other options box
@@ -336,7 +336,7 @@ void BitmapComboBoxWidgetsPage::CreateContent()
     m_textChangeHeight->SetSize(20, wxDefaultCoord);
     sizerOptions->Add(sizerRow, 0, wxALL | wxFIXED_MINSIZE /*| wxGROW*/, 5);
 
-    sizerLeft->Add(sizerOptions, 0, wxGROW | wxALIGN_CENTRE_HORIZONTAL | wxTOP, 2);
+    sizerLeft->Add( sizerOptions, wxSizerFlags().Expand().Border(wxTOP, 2));
 
     // middle pane
     wxStaticBox *box2 = new wxStaticBox(this, wxID_ANY,

samples/widgets/odcombobox.cpp
@@ -348,7 +348,7 @@ void ODComboboxWidgetsPage::CreateContent()
     wxButton *btn = new wxButton(this, ODComboPage_Reset, wxT("&Reset"));
     sizerStyle->Add(btn, 0, wxALIGN_CENTRE_HORIZONTAL | wxALL, 3);
 
-    sizerLeft->Add(sizerStyle, 0, wxGROW | wxALIGN_CENTRE_HORIZONTAL);
+    sizerLeft->Add(sizerStyle, wxSizerFlags().Expand());
 
     // left pane - popup adjustment box
     box = new wxStaticBox(this, wxID_ANY, wxT("Adjust &popup"));
@@ -369,7 +369,7 @@ void ODComboboxWidgetsPage::CreateContent()
 
     m_chkAlignpopupright = CreateCheckBoxAndAddToSizer(sizerPopupPos, wxT("Align Right"));
 
-    sizerLeft->Add(sizerPopupPos, 0, wxGROW | wxALIGN_CENTRE_HORIZONTAL | wxTOP, 2);
+    sizerLeft->Add(sizerPopupPos, wxSizerFlags().Expand().Border(wxTOP, 2));
 
     // left pane - button adjustment box
     box = new wxStaticBox(this, wxID_ANY, wxT("Adjust &button"));
@@ -396,7 +396,7 @@ void ODComboboxWidgetsPage::CreateContent()
 
     m_chkAlignbutleft = CreateCheckBoxAndAddToSizer(sizerButtonPos, wxT("Align Left"));
 
-    sizerLeft->Add(sizerButtonPos, 0, wxGROW | wxALIGN_CENTRE_HORIZONTAL | wxTOP, 2);
+    sizerLeft->Add(sizerButtonPos, wxSizerFlags().Expand().Border(wxTOP, 2));
 
     // middle pane
     wxStaticBox *box2 = new wxStaticBox(this, wxID_ANY,

commit f60fd7467086049c0d99accf8bd66108e95782d1

Use wxStdDialogButtonSizer in generic wxProgressDialog
This improves the placement of the buttons by making it consistent with the
platform conventions and also fixes a regression in button placement
introduced in 5e605217a58551ea680c57c396b7888c0739a060

Closes #17213.

src/generic/progdlgg.cpp
@@ -241,37 +241,33 @@ bool wxGenericProgressDialog::Create( const wxString& title,
     m_btnAbort =
     m_btnSkip = NULL;
 
-    wxBoxSizer *buttonSizer = new wxBoxSizer(wxHORIZONTAL);
+    wxStdDialogButtonSizer *buttonSizer = new wxStdDialogButtonSizer();
 
-    const int borderFlags =
-#if defined(__WXMSW__) || defined(__WXOSX__)
-        wxALL
-#else
-        wxBOTTOM | wxTOP
-#endif
-        ;
+    const int borderFlags = wxALL;
 
-    const wxSizerFlags sizerFlags
+    wxSizerFlags sizerFlags
         = wxSizerFlags().Border(borderFlags, LAYOUT_MARGIN);
 
     if ( HasPDFlag(wxPD_CAN_SKIP) )
     {
         m_btnSkip = new wxButton(this, wxID_SKIP, _("&Skip"));
 
-        buttonSizer->Add(m_btnSkip, sizerFlags);
+        buttonSizer->SetNegativeButton(m_btnSkip);
     }
 
     if ( HasPDFlag(wxPD_CAN_ABORT) )
     {
         m_btnAbort = new wxButton(this, wxID_CANCEL);
 
-        buttonSizer->Add(m_btnAbort, sizerFlags);
+        buttonSizer->AddButton(m_btnAbort);
     }
 
     if ( !HasPDFlag(wxPD_CAN_SKIP | wxPD_CAN_ABORT) )
         buttonSizer->AddSpacer(LAYOUT_MARGIN);
 
-    sizerTop->Add(buttonSizer, sizerFlags);
+    buttonSizer->Realize();
+
+    sizerTop->Add(buttonSizer, sizerFlags.Expand());
 
     SetSizerAndFit(sizerTop);
 
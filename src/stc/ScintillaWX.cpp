Bricsys/wxWidgets

commit 15b542e4cd71fb4646508c4da5706563c1d67ca5

Fix handling of keys with Ctrl modifier in wxSTC under OS X
The real Ctrl key is represented by "meta" modifier bit under OS X as the
"ctrl" bit is used for the Cmd key, but the previously used KeyDown() method
didn't support the meta bit at all, so key combinations using Ctrl didn't
work.

Also remove obsolete code which mapped "meta" to "ctrl", probably predating
the mapping of Cmd to "ctrl" at wx API level.

See #16243.

src/stc/ScintillaWX.cpp
@@ -1037,11 +1037,7 @@ int  ScintillaWX::DoKeyDown(const wxKeyEvent& evt, bool* consumed)
         return 0;
     }
 
-    bool shift = evt.ShiftDown(),
-         ctrl  = evt.ControlDown(),
-         alt   = evt.AltDown();
-
-    if (ctrl && key >= 1 && key <= 26 && key != WXK_BACK)
+    if (evt.RawControlDown() && key >= 1 && key <= 26 && key != WXK_BACK)
         key += 'A' - 1;
 
     switch (key) {
@@ -1082,23 +1078,18 @@ int  ScintillaWX::DoKeyDown(const wxKeyEvent& evt, bool* consumed)
     case WXK_MENU:              key = SCK_MENU; break;
     }
 
-#ifdef __WXMAC__
-    if ( evt.MetaDown() ) {
-        // check for a few common Mac Meta-key combos and remap them to Ctrl
-        // for Scintilla
-        switch ( key ) {
-        case 'Z':       // Undo
-        case 'X':       // Cut
-        case 'C':       // Copy
-        case 'V':       // Paste
-        case 'A':       // Select All
-            ctrl = true;
-            break;
-        }
-    }
-#endif
-
-    int rv = KeyDown(key, shift, ctrl, alt, consumed);
+    int rv = KeyDownWithModifiers
+             (
+                key,
+                ModifierFlags
+                (
+                    evt.ShiftDown(),
+                    evt.ControlDown(),
+                    evt.AltDown(),
+                    evt.RawControlDown()
+                ),
+                consumed
+             );
 
     if (key)
         return rv;

commit 708f9de688c38c77127e67283c31e032258da616

Restore wxSTC built-in key mappings for non-Mac platforms
This was broken by 15b542e4cd71fb4646508c4da5706563c1d67ca5, so keep one of
the changes done there conditional on the platform to avoid changing behaviour
for the non-Mac ones.

Closes #17251.

src/stc/ScintillaWX.cpp
@@ -1105,7 +1105,14 @@ int  ScintillaWX::DoKeyDown(const wxKeyEvent& evt, bool* consumed)
                     evt.ShiftDown(),
                     evt.ControlDown(),
                     evt.AltDown(),
+                    // Under Mac, ControlDown() returns the status of Cmd key,
+                    // so we sneak the status of the real Ctrl key via SCI_META
+                    // but we shouldn't be doing this elsewhere.
+#ifdef __WXMAC__
                     evt.RawControlDown()
+#else
+                    0
+#endif
                 ),
                 consumed
              );
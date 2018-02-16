Bricsys/wxWidgets

commit d3fc8ecbce25ddac6427edbf87d85e83c3fe026f

Apply erans patch to allow building with MinGW

src/msw/graphicsd2d.cpp
@@ -40,6 +40,12 @@
 #include <dwrite.h>
 #include <wincodec.h>
 
+#ifdef __MINGW64_TOOLCHAIN__
+#ifndef DRWITE_E_NOFONT
+#define DWRITE_E_NOFONT _HRESULT_TYPEDEF_(0x88985002)
+#endif
+#endif
+
 #if wxD2D_DEVICE_CONTEXT_SUPPORTED
 #include <D3D11.h>
 #include <D2d1_1.h>

src/stc/PlatWX.cpp
@@ -48,6 +48,8 @@
 
 #if wxUSE_GRAPHICS_DIRECT2D
 #include "ScintillaWX.h"
+#include <float.h>
+#include "wx/dcscreen.h"
 #endif
 
 Point Point::FromLong(long lpoint) {
@@ -732,10 +734,12 @@ SurfaceFontDataD2D::SurfaceFontDataD2D(const FontParameters& fp)
         {
             fontWeight = DWRITE_FONT_WEIGHT_LIGHT;
         }
+#ifndef __MINGW64_TOOLCHAIN__
         else if ( weight <= 350 )
         {
             fontWeight = DWRITE_FONT_WEIGHT_SEMI_LIGHT;
         }
+#endif
         else if ( weight <= 400 )
         {
             fontWeight = DWRITE_FONT_WEIGHT_NORMAL;

commit be75d7e48cc1a6fbdff4b737acb8c013ca70d270

Fix build warnings in Direct2D related code
Regressions since d3fc8ecbce25ddac6427edbf87d85e83c3fe026f and
a372965c56ad473c077c5a756d5bb58eea3b2972.

See https://github.com/wxWidgets/wxWidgets/pull/731

src/msw/graphicsd2d.cpp
@@ -41,8 +41,8 @@
 #include <wincodec.h>
 
 #ifdef __MINGW64_TOOLCHAIN__
-#ifndef DRWITE_E_NOFONT
-#define DWRITE_E_NOFONT _HRESULT_TYPEDEF_(0x88985002)
+#ifndef DWRITE_E_NOFONT
+#define DWRITE_E_NOFONT _HRESULT_TYPEDEF_(0x88985002L)
 #endif
 #endif
 

src/stc/PlatWX.cpp
@@ -797,9 +797,9 @@ bool SurfaceFontDataD2D::Initialised() const
 // SurfaceDataD2D
 
 SurfaceDataD2D::SurfaceDataD2D(ScintillaWX* editor)
-    : m_editor(editor),
-      m_pD2DFactory(::wxD2D1Factory()),
-      m_pDWriteFactory(::wxDWriteFactory())
+    : m_pD2DFactory(::wxD2D1Factory()),
+      m_pDWriteFactory(::wxDWriteFactory()),
+      m_editor(editor)
 {
     if ( Initialised() )
     {
@@ -1107,7 +1107,7 @@ void SurfaceD2D::Release()
             m_clipsActive--;
         }
         hr = m_pRenderTarget->EndDraw();
-        if ( hr == D2DERR_RECREATE_TARGET && m_surfaceData && !m_ownRenderTarget )
+        if ( hr == (HRESULT)D2DERR_RECREATE_TARGET && m_surfaceData && !m_ownRenderTarget )
         {
             m_surfaceData->DiscardGraphicsResources();
             m_surfaceData->SetEditorPaintAbandoned();
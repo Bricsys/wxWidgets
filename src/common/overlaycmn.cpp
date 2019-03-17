Bricsys/wxWidgets

commit f45e032ebeba53c3705bdb96ad5fb591c952e54a

Fix wxDCOverlay ctor associated with the entire wxDC
Use logical coordinates when initializing wxOverlay because this is what it
needs, not physical ones.

See #14415.

src/common/overlaycmn.cpp
@@ -94,10 +94,13 @@ wxDCOverlay::wxDCOverlay(wxOverlay &overlay, wxDC *dc, int x , int y , int width
 wxDCOverlay::wxDCOverlay(wxOverlay &overlay, wxDC *dc) :
     m_overlay(overlay)
 {
-    int width;
-    int height;
-    dc->GetSize(&width,&height);
-    Init(dc, 0, 0, width, height);
+    const wxRect device(wxPoint(0, 0), dc->GetSize());
+
+    Init(dc,
+         dc->DeviceToLogicalX(device.GetLeft()),
+         dc->DeviceToLogicalY(device.GetTop()),
+         dc->DeviceToLogicalX(device.GetRight()),
+         dc->DeviceToLogicalY(device.GetBottom()));
 }
 
 wxDCOverlay::~wxDCOverlay()

commit f907aa458da69ee7f845ae1799d6a0cfc7ff094d

Fix overlay initialization
Fix the size used for a device context initialization, this fixes a
regression from f45e032ebeba53c3705bdb96ad5fb591c952e54a which used
right/bottom coordinate instead of width/height as Init() arguments.

See #14415.

Closes https://github.com/wxWidgets/wxWidgets/pull/1261

src/common/overlaycmn.cpp
@@ -94,13 +94,18 @@ wxDCOverlay::wxDCOverlay(wxOverlay &overlay, wxDC *dc, int x , int y , int width
 wxDCOverlay::wxDCOverlay(wxOverlay &overlay, wxDC *dc) :
     m_overlay(overlay)
 {
-    const wxRect device(wxPoint(0, 0), dc->GetSize());
+    const wxSize size(dc->GetSize());
+
+    const wxCoord logicalLeft = dc->DeviceToLogicalX(0);
+    const wxCoord logicalTop = dc->DeviceToLogicalY(0);
+    const wxCoord logicalRight = dc->DeviceToLogicalX(size.GetWidth());
+    const wxCoord logicalBottom = dc->DeviceToLogicalY(size.GetHeight());
 
     Init(dc,
-         dc->DeviceToLogicalX(device.GetLeft()),
-         dc->DeviceToLogicalY(device.GetTop()),
-         dc->DeviceToLogicalX(device.GetRight()),
-         dc->DeviceToLogicalY(device.GetBottom()));
+         logicalLeft,
+         logicalTop,
+         logicalRight - logicalLeft,
+         logicalBottom - logicalTop);
 }
 
 wxDCOverlay::~wxDCOverlay()
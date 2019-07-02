Bricsys/wxWidgets

commit 1d2a3972afb9ad32b28abeab229f7fbe53661980

Fix width returned by wxSpinCtrl::GetSizeFromTextSize() in wxMSW
Since the changes of df861d9aa8c0cea8dc1ca5d3193c5b99a4f2a618, this
function returned a value which was too small because the code in it
never accounted for the text control margins, but this was hidden by a
weird hack adding extra "3*y/10 + 10" pixels to the horizontal extent
which was removed by the commit above.

Really fix it now by reusing wxTextCtrl::GetSizeFromTextSize() which
correctly accounts for the margins and everything else.

See #12297.

Closes #18391.

src/msw/spinctrl.cpp
@@ -734,21 +734,14 @@ wxSize wxSpinCtrl::DoGetSizeFromTextSize(int xlen, int ylen) const
 {
     wxSize sizeBtn = wxSpinButton::DoGetBestSize();
 
-    int y;
-    wxGetCharSize(GetHWND(), NULL, &y, GetFont());
-    // JACS: we should always use the height calculated
-    // from above, because otherwise we'll get a spin control
-    // that's too big. So never use the height calculated
-    // from wxSpinButton::DoGetBestSize().
+    // Create a temporary wxTextCtrl wrapping our existing HWND in order to be
+    // able to reuse its GetSizeFromTextSize() implementation.
+    wxTextCtrl text;
+    TempHWNDSetter set(&text, m_hwndBuddy);
 
-    wxSize tsize(xlen + sizeBtn.x - GetOverlap(),
-                 EDIT_HEIGHT_FROM_CHAR_HEIGHT(y));
-
-    // Check if the user requested a non-standard height.
-    if ( ylen > 0 )
-        tsize.IncBy(0, ylen - y);
-
-    return tsize;
+    // Increase the width to accommodate the button, which should fit inside
+    // the text control while taking account of the overlap.
+    return text.GetSizeFromTextSize(xlen + sizeBtn.x - GetOverlap(), ylen);
 }
 
 void wxSpinCtrl::DoMoveWindow(int x, int y, int width, int height)

commit 1c64ccf45fe4e748a4a1f0436d09a6244cb66ca6

Fix best size of wxMSW wxSpinCtrl with non-default font
This has been broken by 1d2a3972afb9ad32b28abeab229f7fbe53661980 after
which the actual font used for the control was not taken into account
any longer. Fix this by ensuring the wxTextCtrl used for size
calculation uses the same font as wxSpinCtrl itself.

See #12297, #18391.

Closes #18427.

src/msw/spinctrl.cpp
@@ -739,6 +739,11 @@ wxSize wxSpinCtrl::DoGetSizeFromTextSize(int xlen, int ylen) const
     wxTextCtrl text;
     TempHWNDSetter set(&text, m_hwndBuddy);
 
+    // It's unnecessary to actually change the font used by the buddy control,
+    // but we do need to ensure that the helper wxTextCtrl wxFont matches what
+    // it is used as GetSizeFromTextSize() uses the current font.
+    text.wxWindowBase::SetFont(GetFont());
+
     // Increase the width to accommodate the button, which should fit inside
     // the text control while taking account of the overlap.
     return text.GetSizeFromTextSize(xlen + sizeBtn.x - GetOverlap(), ylen);
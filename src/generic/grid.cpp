Bricsys/wxWidgets

commit 16d53a4dc0614a72055c7d771151b8d72ca1dd41

Fix auto-sizing column labels when using native header in wxGrid
Use wxHeaderCtrl-specific GetColumnTitleWidth() function to account for
the native header control margins, otherwise the computed width could be
insufficient for short columns, resulting in their ellipsization.

src/generic/grid.cpp
@@ -9384,12 +9384,29 @@ wxGrid::AutoSizeColOrRow(int colOrRow, bool setAsMin, wxGridDirection direction)
 
     if ( column )
     {
-        dc.GetMultiLineTextExtent( GetColLabelValue(colOrRow), &w, &h );
-        if ( GetColLabelTextOrientation() == wxVERTICAL )
-            w = h;
+        if ( m_useNativeHeader )
+        {
+            w = GetGridColHeader()->GetColumnTitleWidth(colOrRow);
+            h = 0;
+        }
+        else
+        {
+            dc.GetMultiLineTextExtent( GetColLabelValue(colOrRow), &w, &h );
+            if ( GetColLabelTextOrientation() == wxVERTICAL )
+                w = h;
+
+            // leave some space around text
+            if ( w )
+                w += 10;
+        }
     }
     else
+    {
         dc.GetMultiLineTextExtent( GetRowLabelValue(colOrRow), &w, &h );
+
+        if ( h )
+            h += 6;
+    }
 
     extent = column ? w : h;
     if ( extent > extentMax )
@@ -9400,14 +9417,6 @@ wxGrid::AutoSizeColOrRow(int colOrRow, bool setAsMin, wxGridDirection direction)
         // empty column - give default extent (notice that if extentMax is less
         // than default extent but != 0, it's OK)
         extentMax = column ? m_defaultColWidth : m_defaultRowHeight;
-    }
-    else
-    {
-        if ( column )
-            // leave some space around text
-            extentMax += 10;
-        else
-            extentMax += 6;
     }
 
     if ( column )

commit 04a0d43ff18983355ad9a1cdab4e7e26bfda1439

Fix recent regression in grid content autosizing
Fix autosizing broken in 16d53a4dc0614a72055c7d771151b8d72ca1dd41: we
must add the extra margin to "extentMax".

See https://github.com/wxWidgets/wxWidgets/pull/1559

Closes https://github.com/wxWidgets/wxWidgets/pull/1578

src/generic/grid.cpp
@@ -9378,11 +9378,16 @@ wxGrid::AutoSizeColOrRow(int colOrRow, bool setAsMin, wxGridDirection direction)
     wxCoord w, h;
     dc.SetFont( GetLabelFont() );
 
+    bool addMargin = true;
+
     if ( column )
     {
         if ( m_useNativeHeader )
         {
             w = GetGridColHeader()->GetColumnTitleWidth(colOrRow);
+
+            // GetColumnTitleWidth already adds margins internally.
+            addMargin = false;
             h = 0;
         }
         else
@@ -9390,18 +9395,11 @@ wxGrid::AutoSizeColOrRow(int colOrRow, bool setAsMin, wxGridDirection direction)
             dc.GetMultiLineTextExtent( GetColLabelValue(colOrRow), &w, &h );
             if ( GetColLabelTextOrientation() == wxVERTICAL )
                 w = h;
-
-            // leave some space around text
-            if ( w )
-                w += 10;
         }
     }
     else
     {
         dc.GetMultiLineTextExtent( GetRowLabelValue(colOrRow), &w, &h );
-
-        if ( h )
-            h += 6;
     }
 
     extent = column ? w : h;
@@ -9413,6 +9411,14 @@ wxGrid::AutoSizeColOrRow(int colOrRow, bool setAsMin, wxGridDirection direction)
         // empty column - give default extent (notice that if extentMax is less
         // than default extent but != 0, it's OK)
         extentMax = column ? m_defaultColWidth : m_defaultRowHeight;
+    }
+    else if ( addMargin )
+    {
+        // leave some space around text
+        if ( column )
+            extentMax += 10;
+        else
+            extentMax += 6;
     }
 
     if ( column )
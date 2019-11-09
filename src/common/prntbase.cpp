Bricsys/wxWidgets

commit 1b72eb20f9b0a27a6aa2dd5d654cf7e3056e480d

Add wxPrintout::SetUp() to reuse the same code in all ports
MSW, GTK, OSX and Postscript implementations all did almost exactly the
same thing to initialize wxPrintout, so extract this common code into a
new wxPrintout method and just call it instead.

There should be no changes in behaviour.

src/common/prntbase.cpp
@@ -638,6 +638,33 @@ void wxPrintout::GetPageInfo(int *minPage, int *maxPage, int *fromPage, int *toP
     *toPage = 1;
 }
 
+bool wxPrintout::SetUp(wxDC& dc)
+{
+    SetPPIScreen(wxGetDisplayPPI());
+
+    // We need to know printer PPI. In most ports, this can be retrieved from
+    // the printer DC, but in others it is computed (probably for legacy
+    // reasons) outside of wxDC code, so don't override it if it had been
+    // already set.
+    if ( !m_PPIPrinterX || !m_PPIPrinterY )
+    {
+        SetPPIPrinter(dc.GetPPI());
+        if ( !m_PPIPrinterX || !m_PPIPrinterY )
+        {
+            // But if we couldn't get it in any way, we can't continue.
+            return false;
+        }
+    }
+
+    SetDC(&dc);
+
+    dc.GetSize(&m_pageWidthPixels, &m_pageHeightPixels);
+    m_paperRectPixels = wxRect(0, 0, m_pageWidthPixels, m_pageHeightPixels);
+    dc.GetSizeMM(&m_pageWidthMM, &m_pageHeightMM);
+
+    return true;
+}
+
 void wxPrintout::FitThisSizeToPaper(const wxSize& imageSize)
 {
     // Set the DC scale and origin so that the given image size fits within the

include/wx/prntbase.h
@@ -281,6 +281,10 @@ public:
 
     virtual wxString GetTitle() const { return m_printoutTitle; }
 
+    // Port-specific code should call this function to initialize this object
+    // with everything it needs, instead of using individual accessors below.
+    bool SetUp(wxDC& dc);
+
     wxDC *GetDC() const { return m_printoutDC; }
     void SetDC(wxDC *dc) { m_printoutDC = dc; }
 

src/generic/printps.cpp
@@ -105,24 +105,8 @@ bool wxPostScriptPrinter::Print(wxWindow *parent, wxPrintout *printout, bool pro
         return false;
     }
 
-    wxSize ScreenPixels = wxGetDisplaySize();
-    wxSize ScreenMM = wxGetDisplaySizeMM();
-
-    printout->SetPPIScreen( (int) ((ScreenPixels.GetWidth() * 25.4) / ScreenMM.GetWidth()),
-                            (int) ((ScreenPixels.GetHeight() * 25.4) / ScreenMM.GetHeight()) );
-    printout->SetPPIPrinter( dc->GetResolution(),
-                             dc->GetResolution() );
-
     // Set printout parameters
-    printout->SetDC(dc);
-
-    int w, h;
-    dc->GetSize(&w, &h);
-    printout->SetPageSizePixels((int)w, (int)h);
-    printout->SetPaperRectPixels(wxRect(0, 0, w, h));
-    int mw, mh;
-    dc->GetSizeMM(&mw, &mh);
-    printout->SetPageSizeMM((int)mw, (int)mh);
+    printout->SetUp(*dc);
 
     // Create an abort window
     wxBeginBusyCursor();

src/gtk/print.cpp
@@ -1024,19 +1024,8 @@ void wxGtkPrinter::BeginPrint(wxPrintout *printout, GtkPrintOperation *operation
         return;
     }
 
-    printout->SetPPIScreen(wxGetDisplayPPI());
-    printout->SetPPIPrinter( printDC->GetResolution(),
-                             printDC->GetResolution() );
+    printout->SetUp(*m_dc);
 
-    printout->SetDC(m_dc);
-
-    int w, h;
-    m_dc->GetSize(&w, &h);
-    printout->SetPageSizePixels((int)w, (int)h);
-    printout->SetPaperRectPixels(wxRect(0, 0, w, h));
-    int mw, mh;
-    m_dc->GetSizeMM(&mw, &mh);
-    printout->SetPageSizeMM((int)mw, (int)mh);
     printout->OnPreparePrinting();
 
     // Get some parameters from the printout, if defined.

src/msw/printwin.cpp
@@ -115,35 +115,13 @@ bool wxWindowsPrinter::Print(wxWindow *parent, wxPrintout *printout, bool prompt
         return false;
     }
 
-    wxPrinterDCImpl *impl = (wxPrinterDCImpl*) dc->GetImpl();
-
-    HDC hdc = ::GetDC(NULL);
-    int logPPIScreenX = ::GetDeviceCaps(hdc, LOGPIXELSX);
-    int logPPIScreenY = ::GetDeviceCaps(hdc, LOGPIXELSY);
-    ::ReleaseDC(NULL, hdc);
-
-    int logPPIPrinterX = ::GetDeviceCaps((HDC) impl->GetHDC(), LOGPIXELSX);
-    int logPPIPrinterY = ::GetDeviceCaps((HDC) impl->GetHDC(), LOGPIXELSY);
-    if (logPPIPrinterX == 0 || logPPIPrinterY == 0)
+    // Set printout parameters
+    if (!printout->SetUp(*dc))
     {
         delete dc;
         sm_lastError = wxPRINTER_ERROR;
         return false;
     }
-
-    printout->SetPPIScreen(logPPIScreenX, logPPIScreenY);
-    printout->SetPPIPrinter(logPPIPrinterX, logPPIPrinterY);
-
-    // Set printout parameters
-    printout->SetDC(dc);
-
-    int w, h;
-    dc->GetSize(&w, &h);
-    printout->SetPageSizePixels((int)w, (int)h);
-    printout->SetPaperRectPixels(dc->GetPaperRect());
-
-    dc->GetSizeMM(&w, &h);
-    printout->SetPageSizeMM((int)w, (int)h);
 
     // Create an abort window
     wxBusyCursor busyCursor;
@@ -169,7 +147,7 @@ bool wxWindowsPrinter::Print(wxWindow *parent, wxPrintout *printout, bool prompt
     wxPrintAbortDialog *win = CreateAbortWindow(parent, printout);
     wxYield();
 
-    ::SetAbortProc(GetHdcOf(*impl), wxAbortProc);
+    ::SetAbortProc(GetHdcOf(*dc), wxAbortProc);
 
     if (!win)
     {

src/osx/core/printmac.cpp
@@ -585,8 +585,6 @@ bool wxMacPrinter::Print(wxWindow *parent, wxPrintout *printout, bool prompt)
         return false;
     }
 
-    printout->SetPPIScreen(wxGetDisplayPPI());
-
     PMResolution res;
     PMPrinter printer;
     wxOSXPrintData* nativeData = (wxOSXPrintData*)
@@ -607,15 +605,7 @@ bool wxMacPrinter::Print(wxWindow *parent, wxPrintout *printout, bool prompt)
     printout->SetPPIPrinter(int(res.hRes), int(res.vRes));
 
     // Set printout parameters
-    printout->SetDC(dc);
-
-    int w, h;
-    dc->GetSize(&w, &h);
-    printout->SetPageSizePixels((int)w, (int)h);
-    printout->SetPaperRectPixels(dc->GetPaperRect());
-    wxCoord mw, mh;
-    dc->GetSizeMM(&mw, &mh);
-    printout->SetPageSizeMM((int)mw, (int)mh);
+    printout->SetUp(*dc);
 
     // Create an abort window
     wxBeginBusyCursor();

commit ba48724088d09fc6cd1b30e0ed7ac8ff9ec3874b

Fix paper size correctly for printouts under MSW and Mac
This was broken by 1b72eb20f9b0a27a6aa2dd5d654cf7e3056e480d which wasn't
supposed to change anything, but did because it effectively replaced the
call to wxDC::GetPaperRect() with wxDC::GetSize() when initializing
wxPrintout::m_paperRectPixels, and while GetPaperRect() is the same as
GetSize() in wxGTK, it returns the rectangle including non-printable
margins in wxMSW and wxOSX, and must be used here instead.

Closes #18565.

Closes https://github.com/wxWidgets/wxWidgets/pull/1640

src/common/prntbase.cpp
@@ -640,6 +640,8 @@ void wxPrintout::GetPageInfo(int *minPage, int *maxPage, int *fromPage, int *toP
 
 bool wxPrintout::SetUp(wxDC& dc)
 {
+    wxCHECK_MSG( dc.IsOk(), false, "should have a valid DC to set up" );
+
     SetPPIScreen(wxGetDisplayPPI());
 
     // We need to know printer PPI. In most ports, this can be retrieved from
@@ -659,7 +661,10 @@ bool wxPrintout::SetUp(wxDC& dc)
     SetDC(&dc);
 
     dc.GetSize(&m_pageWidthPixels, &m_pageHeightPixels);
-    m_paperRectPixels = wxRect(0, 0, m_pageWidthPixels, m_pageHeightPixels);
+    // This is ugly, but wxDCImpl itself has GetPaperRect() method while wxDC
+    // doesn't (only wxPrinterDC does), so use GetImpl() to avoid having to use
+    // a dynamic cast.
+    m_paperRectPixels = dc.GetImpl()->GetPaperRect();
     dc.GetSizeMM(&m_pageWidthMM, &m_pageHeightMM);
 
     return true;
Bricsys/wxWidgets

commit c2707e1b28161f75c53a0572eba9904bca1ef9e7

Call wxAuiManager::UnInit() if associated frame is destroyed
Don't require the application code to explicitly do it if the frame is
destroyed before the manager itself.

src/aui/framemanager.cpp
@@ -593,6 +593,7 @@ bool wxAuiPaneInfo::IsValid() const
 wxBEGIN_EVENT_TABLE(wxAuiManager, wxEvtHandler)
     EVT_AUI_PANE_BUTTON(wxAuiManager::OnPaneButton)
     EVT_AUI_RENDER(wxAuiManager::OnRender)
+    EVT_WINDOW_DESTROY(wxAuiManager::OnDestroy)
     EVT_PAINT(wxAuiManager::OnPaint)
     EVT_ERASE_BACKGROUND(wxAuiManager::OnEraseBackground)
     EVT_SIZE(wxAuiManager::OnSize)
@@ -914,6 +915,8 @@ void wxAuiManager::SetManagedWindow(wxWindow* wnd)
 {
     wxASSERT_MSG(wnd, wxT("specified window must be non-NULL"));
 
+    UnInit();
+
     m_frame = wnd;
     m_frame->PushEventHandler(this);
 
@@ -958,6 +961,7 @@ void wxAuiManager::UnInit()
     if (m_frame)
     {
         m_frame->RemoveEventHandler(this);
+        m_frame = NULL;
     }
 }
 
@@ -3949,6 +3953,12 @@ void wxAuiManager::Repaint(wxDC* dc)
     // if we created a client_dc, delete it
     if (client_dc)
         delete client_dc;
+}
+
+void wxAuiManager::OnDestroy(wxWindowDestroyEvent& event)
+{
+    if ( event.GetEventObject() == m_frame )
+        UnInit();
 }
 
 void wxAuiManager::OnPaint(wxPaintEvent& WXUNUSED(event))

include/wx/aui/framemanager.h
@@ -555,6 +555,7 @@ public:
 protected:
 
     // protected events
+    void OnDestroy(wxWindowDestroyEvent& evt);
     void OnPaint(wxPaintEvent& evt);
     void OnEraseBackground(wxEraseEvent& evt);
     void OnSize(wxSizeEvent& evt);

commit 40ac197a10301f368a127172568b747de66b1b6d

Send wxEVT_DESTROY to children of wxAuiManager-associated window
This fixes a regression introduced in c2707e1b28 (Call
wxAuiManager::UnInit() if associated frame is destroyed, 2020-07-18)
since when children of a window managed by wxAuiManager didn't receive
wxEVT_DESTROY any more because the manager intercepted it for its own
needs.

Closes #18938.

src/aui/framemanager.cpp
@@ -3961,7 +3961,23 @@ void wxAuiManager::Repaint(wxDC* dc)
 void wxAuiManager::OnDestroy(wxWindowDestroyEvent& event)
 {
     if ( event.GetEventObject() == m_frame )
+    {
+        wxWindow* const frame = m_frame;
+
         UnInit();
+
+        // Just calling Skip() would be insufficient in this case, as by
+        // removing the event handler from the event handlers chain in UnInit()
+        // we'd still prevent the frame from getting this event, so we need to
+        // forward it to it manually. Note that this must be done after calling
+        // UnInit() to prevent infinite recursion.
+        if ( frame )
+            frame->ProcessWindowEventLocally(event);
+    }
+    else
+    {
+        event.Skip();
+    }
 }
 
 void wxAuiManager::OnPaint(wxPaintEvent& WXUNUSED(event))
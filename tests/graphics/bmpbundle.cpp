Bricsys/wxWidgets

commit 8fd55330d90cd2c9632ade3567188968c69e4872

Avoid unnecessarily unsharing bitmaps in wxMSW
Only call AllocExclusive() if really need to modify the bitmap and avoid
doing it if the scale factor doesn't actually change, as this saves on
unnecessary expensive bitmap copy.

This also avoids the problem of getting wrong background for SVG images
since this call to AllocExclusive() was added in 350f25f1b7 (Ensure
wxBitmap::SetScaleFactor() doesn't change bitmap copies, 2022-01-22)
when not using high DPI, although the problem still remains in the high
DPI case as bitmap alpha channel is still copied incorrectly when we do
have to copy it.

src/msw/gdiimage.cpp
@@ -346,9 +346,13 @@ void wxGDIImage::SetScaleFactor(double scale)
 {
     wxCHECK_RET( IsOk(), wxT("invalid bitmap") );
 
-    AllocExclusive();
+    double& scaleCurrent = GetGDIImageData()->m_scaleFactor;
+    if ( scale != scaleCurrent )
+    {
+        AllocExclusive();
 
-    GetGDIImageData()->m_scaleFactor = scale;
+        scaleCurrent = scale;
+    }
 }
 
 double wxGDIImage::GetScaleFactor() const

commit a702807f8eb112d1d7e43bbafe17d1ad7eeeab91

Really fix setting wxBitmap scale factor
Optimization of 8fd55330d9 (Avoid unnecessarily unsharing bitmaps in
wxMSW, 2022-02-02) broke setting the scale factor because we didn't
update the right bitmap data in case bitmap hadn't had exclusive
ownership of its data before. Fix this by not using a reference which
may refer to another object after AllocExclusive() call.

Also change the other ports to unshared the bitmap when modifying its
scale factor so that the new unit test passes for them too.

src/gtk/bitmap.cpp
@@ -1001,7 +1001,12 @@ void wxBitmap::SetScaleFactor(double scale)
 {
     wxCHECK_RET(m_refData, "invalid bitmap");
 
-    M_BMPDATA->m_scaleFactor = scale;
+    if ( M_BMPDATA->m_scaleFactor != scale )
+    {
+        AllocExclusive();
+
+        M_BMPDATA->m_scaleFactor = scale;
+    }
 }
 
 double wxBitmap::GetScaleFactor() const

src/msw/gdiimage.cpp
@@ -346,12 +346,13 @@ void wxGDIImage::SetScaleFactor(double scale)
 {
     wxCHECK_RET( IsOk(), wxT("invalid bitmap") );
 
-    double& scaleCurrent = GetGDIImageData()->m_scaleFactor;
-    if ( scale != scaleCurrent )
+    if ( GetGDIImageData()->m_scaleFactor != scale )
     {
         AllocExclusive();
 
-        scaleCurrent = scale;
+        // Note that GetGDIImageData() result may have changed after calling
+        // AllocExclusive(), so don't be tempted to optimize it.
+        GetGDIImageData()->m_scaleFactor = scale;
     }
 }
 

src/osx/core/bitmap.cpp
@@ -1391,7 +1391,12 @@ void wxBitmap::SetScaleFactor(double scale)
 {
     wxCHECK_RET( IsOk(), wxT("invalid bitmap") );
 
-    return GetBitmapData()->SetScaleFactor(scale) ;
+    if ( GetBitmapData()->GetScaleFactor() != scale )
+    {
+        AllocExclusive();
+
+        GetBitmapData()->SetScaleFactor(scale) ;
+    }
 }
 
 double wxBitmap::GetScaleFactor() const

tests/graphics/bmpbundle.cpp
@@ -159,3 +159,27 @@ TEST_CASE("BitmapBundle::ArtProvider", "[bmpbundle][art]")
     CHECK( b.IsOk() );
     CHECK( b.GetDefaultSize() == size );
 }
+
+// This test only makes sense for the ports that actually support scaled
+// bitmaps, which is the case for the ports using real logical pixels (they
+// have to support bitmap scale for things to work) and MSW, which doesn't, but
+// still implements support for at least storing and retrieving bitmap scale in
+// its wxBitmap.
+#if defined(wxHAS_DPI_INDEPENDENT_PIXELS) || defined(__WXMSW__)
+
+TEST_CASE("BitmapBundle::Scale", "[bmpbundle][scale]")
+{
+    // This is not a wxBitmapBundle test, strictly speaking, but check that
+    // setting scale factor works correctly for bitmaps, as wxBitmapBundle does
+    // this internally.
+    wxBitmap bmp(16, 16);
+    bmp.SetScaleFactor(2);
+    CHECK( bmp.GetScaleFactor() == 2 );
+
+    wxBitmap bmp2(bmp);
+    bmp.SetScaleFactor(3);
+    CHECK( bmp2.GetScaleFactor() == 2 );
+    CHECK( bmp.GetScaleFactor() == 3 );
+}
+
+#endif // ports with scaled bitmaps support
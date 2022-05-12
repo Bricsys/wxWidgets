Bricsys/wxWidgets

commit f6152008e9195468226abe65bd7e30919d3de5a7

Make native dialogs application-modal in wxMSW
Being able to use other application windows while a modal dialog was
shown was unexpected and could easily result in nasty reentrancies, and
was also inconsistent with the behaviour of ShowModal() for the dialogs
defined in wx, so disable all the windows, not just the parent one,
while showing the native modal dialogs too.

Closes #11887.

src/msw/colordlg.cpp
@@ -177,6 +177,8 @@ int wxColourDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
+    wxWindowDisabler disableOthers(this);
+
     wxWindow* const parent = GetParentForModalDialog(m_parent, GetWindowStyle());
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;
 

src/msw/dirdlg.cpp
@@ -152,6 +152,8 @@ int wxDirDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
+    wxWindowDisabler disableOthers(this);
+
     wxWindow* const parent = GetParentForModalDialog();
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;
 

src/msw/filedlg.cpp
@@ -430,6 +430,8 @@ int wxFileDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
+    wxWindowDisabler disableOthers(this);
+
     wxWindow* const parent = GetParentForModalDialog(m_parent, GetWindowStyle());
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;
 

src/msw/fontdlg.cpp
@@ -92,6 +92,8 @@ int wxFontDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
+    wxWindowDisabler disableOthers(this);
+
     wxWindow* const parent = GetParentForModalDialog(m_parent, GetWindowStyle());
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;
     // It should be OK to always use GDI simulations

src/msw/msgdlg.cpp
@@ -539,6 +539,8 @@ int wxMessageDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
+    wxWindowDisabler disableOthers(this);
+
 #ifdef wxHAS_MSW_TASKDIALOG
     if ( HasNativeTaskDialog() )
     {

src/msw/printdlg.cpp
@@ -29,6 +29,7 @@
     #include "wx/app.h"
     #include "wx/dcprint.h"
     #include "wx/cmndata.h"
+    #include "wx/utils.h"                   // for wxWindowDisabler
 #endif
 
 #include "wx/printdlg.h"
@@ -804,6 +805,8 @@ wxWindowsPrintDialog::~wxWindowsPrintDialog()
 int wxWindowsPrintDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
+
+    wxWindowDisabler disableOthers(this);
 
     wxWindow* const parent = GetParentForModalDialog(m_parent, GetWindowStyle());
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;

src/msw/richmsgdlg.cpp
@@ -17,6 +17,7 @@
 
 #ifndef WX_PRECOMP
     #include "wx/msw/private.h"
+    #include "wx/utils.h"                   // for wxWindowDisabler
 #endif
 
 // This will define wxHAS_MSW_TASKDIALOG if we have support for it in the
@@ -36,6 +37,8 @@ int wxRichMessageDialog::ShowModal()
 
     if ( HasNativeTaskDialog() )
     {
+        wxWindowDisabler disableOthers(this);
+
         // create a task dialog
         WinStruct<TASKDIALOGCONFIG> tdc;
         wxMSWTaskDialogConfig wxTdc(*this);

commit 5084f4d4bbaa23230085c9eb27a2c4e3c5578745

Skip disabling the correct window when showing MSW modal dialogs
The fix of bfe1943e67 (Fix restoring focus after showing native modal
dialogs in wxMSW, 2022-04-07) wasn't quite correct as it used the parent
window specified when showing the dialog, but it should have been using
the effective dialog parent, i.e. owner window in MSW terms.

Fix this by passing GetParentForModalDialog() to wxWindowDisabler, which
ensures that the preserve the same behaviour as before, including
refreshing the owner window after the dialog as dismissed -- which turns
out to be important because some existing code relies on this happening,
see #22362.

This commit should hopefully fix the last regression after f6152008e9
(Make native dialogs application-modal in wxMSW, 2022-04-01) and still
solve the original problem (see #11887), while preserving the original
behaviour when there is just one top-level window.

src/msw/colordlg.cpp
@@ -177,10 +177,10 @@ int wxColourDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
-    wxWindowDisabler disableOthers(this, GetParent());
-
     wxWindow* const parent = GetParentForModalDialog(m_parent, GetWindowStyle());
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;
+
+    wxWindowDisabler disableOthers(this, parent);
 
     // initialize the struct used by Windows
     CHOOSECOLOR chooseColorStruct;

src/msw/dirdlg.cpp
@@ -152,10 +152,10 @@ int wxDirDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
-    wxWindowDisabler disableOthers(this, GetParent());
-
     wxWindow* const parent = GetParentForModalDialog();
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;
+
+    wxWindowDisabler disableOthers(this, parent);
 
     m_paths.clear();
 

src/msw/filedlg.cpp
@@ -430,10 +430,10 @@ int wxFileDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
-    wxWindowDisabler disableOthers(this, GetParent());
-
     wxWindow* const parent = GetParentForModalDialog(m_parent, GetWindowStyle());
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;
+
+    wxWindowDisabler disableOthers(this, parent);
 
     static wxChar fileNameBuffer [ wxMAXPATH ];           // the file-name
     wxChar        titleBuffer    [ wxMAXFILE+1+wxMAXEXT ];  // the file-name, without path

src/msw/fontdlg.cpp
@@ -101,10 +101,11 @@ int wxFontDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
-    wxWindowDisabler disableOthers(this, GetParent());
-
     wxWindow* const parent = GetParentForModalDialog(m_parent, GetWindowStyle());
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;
+
+    wxWindowDisabler disableOthers(this, parent);
+
     // It should be OK to always use GDI simulations
     DWORD flags = CF_SCREENFONTS /* | CF_NOSIMULATIONS */ ;
 

src/msw/msgdlg.cpp
@@ -540,7 +540,7 @@ int wxMessageDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
-    wxWindowDisabler disableOthers(this, GetParent());
+    wxWindowDisabler disableOthers(this, GetParentForModalDialog());
 
 #ifdef wxHAS_MSW_TASKDIALOG
     if ( HasNativeTaskDialog() )

src/msw/printdlg.cpp
@@ -806,10 +806,10 @@ int wxWindowsPrintDialog::ShowModal()
 {
     WX_HOOK_MODAL_DIALOG();
 
-    wxWindowDisabler disableOthers(this, GetParent());
-
     wxWindow* const parent = GetParentForModalDialog(m_parent, GetWindowStyle());
     WXHWND hWndParent = parent ? GetHwndOf(parent) : NULL;
+
+    wxWindowDisabler disableOthers(this, parent);
 
     ConvertToNative( m_printDialogData );
 

src/msw/richmsgdlg.cpp
@@ -37,7 +37,7 @@ int wxRichMessageDialog::ShowModal()
 
     if ( HasNativeTaskDialog() )
     {
-        wxWindowDisabler disableOthers(this, GetParent());
+        wxWindowDisabler disableOthers(this, GetParentForModalDialog());
 
         // create a task dialog
         WinStruct<TASKDIALOGCONFIG> tdc;
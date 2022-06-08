Bricsys/wxWidgets

commit 8b3c3a7621878e5bb61ec312a9f013d5498179c3

Fix loading colour palette from BMP file
Only BITMAPINFOHEADER data are explicitly read and processed so for a bitmap
with BITMAPV4HEADER or BITMAPV5HEADER we have to forward stream position
to the end of the actual header in order to start reading colour table
from the right position.

Closes #18634.

src/common/imagbmp.cpp
@@ -1156,6 +1156,14 @@ bool wxBMPHandler::LoadDib(wxImage *image, wxInputStream& stream,
 
         ncolors = wxINT32_SWAP_ON_BE( (int)dbuf[0] );
         res.Init(dbuf[2]/100, dbuf[3]/100);
+
+        // We've read BITMAPINFOHEADER data but for BITMAPV4HEADER or BITMAPV5HEADER
+        // we have to forward stream position to after the actual bitmap header.
+        if ( hdrSize > sizeof(BITMAPINFOHEADER) )
+        {
+            if ( stream.SeekI(hdrSize - sizeof(BITMAPINFOHEADER), wxFromCurrent) == wxInvalidOffset )
+                return false;
+        }
     }
     if (ncolors == 0)
         ncolors = 1 << bpp;

commit d0ad11df46e74d36de5000868e61d1f2f4c3bde2

Only read BMP pixel data from stream in LoadBMPData()
This makes the code much more clear: LoadDib() now reads all the header
data while LoadBMPData() reads bitmap data only and doesn't read any
header values any more, unlike before.

It also allows not to have to pass neither bmpOffset nor IsBmp
parameters to LoadBMPData(), making its argument list much more
reasonable.

Finally, and, most importantly, it fixes loading bitmaps using
BI_BITFIELDS that was broken back in 8b3c3a7621 (Fix loading colour
palette from BMP file, 2019-12-28).

This commit also adds a unit test checking that this doesn't get broken
again without anyone noticing.

See #18634.

Closes #22499.

src/common/imagbmp.cpp
@@ -508,12 +508,16 @@ struct BMPDesc
     int comp; // BI_RGB, BI_RLE4, BI_RLE8 or BI_BITFIELDS only supported
 
     wxScopedArray<BMPPalette> paletteData;
+
+    int rmask, gmask, bmask, amask;
 };
 
 // Read the data in BMP format into the given image.
+//
+// The stream must be positioned at the start of the palette data for the
+// bitmaps using palettes or at the start of the bitmap data otherwise.
 bool LoadBMPData(wxImage * image, const BMPDesc& desc,
-                             wxFileOffset bmpOffset, wxInputStream& stream,
-                             bool verbose, bool IsBmp)
+                 wxInputStream& stream, bool verbose)
 {
     const int width = desc.width;
     int height = desc.height;
@@ -524,7 +528,6 @@ bool LoadBMPData(wxImage * image, const BMPDesc& desc,
     wxInt32         aDword, rmask = 0, gmask = 0, bmask = 0, amask = 0;
     int             rshift = 0, gshift = 0, bshift = 0, ashift = 0;
     int             rbits = 0, gbits = 0, bbits = 0;
-    wxInt32         dbuf[4];
     wxInt8          bbuf[4];
     wxUint8         aByte;
     wxUint16        aWord;
@@ -611,12 +614,11 @@ bool LoadBMPData(wxImage * image, const BMPDesc& desc,
         if ( desc.comp == BI_BITFIELDS )
         {
             int bit;
-            if ( !stream.ReadAll(dbuf, 4 * 3) )
-                return false;
 
-            rmask = wxINT32_SWAP_ON_BE(dbuf[0]);
-            gmask = wxINT32_SWAP_ON_BE(dbuf[1]);
-            bmask = wxINT32_SWAP_ON_BE(dbuf[2]);
+            rmask = desc.rmask;
+            gmask = desc.gmask;
+            bmask = desc.bmask;
+
             // find shift amount (Least significant bit of mask)
             for (bit = bpp-1; bit>=0; bit--)
             {
@@ -670,18 +672,6 @@ bool LoadBMPData(wxImage * image, const BMPDesc& desc,
     /*
      * Reading the image data
      */
-    if ( IsBmp )
-    {
-        // NOTE: seeking a positive amount in wxFromCurrent mode allows us to
-        //       load even non-seekable streams (see wxInputStream::SeekI docs)!
-        const wxFileOffset pos = stream.TellI();
-        if ( pos == wxInvalidOffset ||
-             (bmpOffset > pos &&
-              stream.SeekI(bmpOffset - pos, wxFromCurrent) == wxInvalidOffset) )
-            return false;
-        //else: icon, just carry on
-    }
-
     unsigned char *data = ptr;
 
     /* set the whole image to the background color */
@@ -1028,7 +1018,7 @@ bool wxBMPHandler::LoadDib(wxImage *image, wxInputStream& stream,
         if ( !stream.ReadAll(dbuf, 4) )
             return false;
 
-        offset = wxInvalidOffset; // not used in loading ICO/CUR DIBs
+        offset = 0; // not used in loading ICO/CUR DIBs
         hdrSize = wxINT32_SWAP_ON_BE(dbuf[0]);
     }
 
@@ -1205,18 +1195,40 @@ bool wxBMPHandler::LoadDib(wxImage *image, wxInputStream& stream,
             return false;
         }
 
-        // We've read BITMAPINFOHEADER data but for BITMAPV4HEADER or BITMAPV5HEADER
-        // we have to forward stream position to after the actual bitmap header.
+        // We've read all BITMAPINFOHEADER data so far, but we may have to
+        // forward the stream to after the actual bitmap header as there could
+        // be more BITMAPV4HEADER or BITMAPV5HEADER fields following.
         //
         // Note: hardcode its size as struct BITMAPINFOHEADER is not defined on
         // non-MSW platforms.
-        const wxInt32 sizeBITMAPINFOHEADER = 40;
-        if ( hdrSize > sizeBITMAPINFOHEADER )
+        wxInt32 hdrBytesRead = 40 /* sizeof(BITMAPINFOHEADER) */;
+
+        if ( desc.comp == BI_BITFIELDS )
         {
-            if ( stream.SeekI(hdrSize - sizeBITMAPINFOHEADER, wxFromCurrent) == wxInvalidOffset )
+            // Read the mask values from the header.
+            if ( !stream.ReadAll(dbuf, 4 * 4) )
+                return false;
+
+            hdrBytesRead += 4 * 4;
+
+            desc.rmask = wxINT32_SWAP_ON_BE(dbuf[0]);
+            desc.gmask = wxINT32_SWAP_ON_BE(dbuf[1]);
+            desc.bmask = wxINT32_SWAP_ON_BE(dbuf[2]);
+            desc.amask = wxINT32_SWAP_ON_BE(dbuf[3]);
+        }
+
+        // Now that we've read everything we needed from the header, advance
+        // past it.
+        if ( hdrSize > hdrBytesRead )
+        {
+            if ( stream.SeekI(hdrSize - hdrBytesRead, wxFromCurrent) == wxInvalidOffset )
                 return false;
         }
     }
+
+    // We must have read the header entirely by now and we also read the 14
+    // bytes preceding it: "BM" signature and 3 other DWORDs.
+    wxFileOffset bytesRead = 14 + hdrSize;
 
     // We must have a palette for 1bpp, 4bpp and 8bpp bitmaps.
     if (desc.ncolors == 0 && desc.bpp < 16)
@@ -1234,6 +1246,8 @@ bool wxBMPHandler::LoadDib(wxImage *image, wxInputStream& stream,
         if ( !stream.ReadAll(data, paletteSize) )
             return false;
 
+        bytesRead += paletteSize;
+
         // Copy it into the format the existing code uses: this could probably
         // be optimized to avoid copying, but palette size is small enough for
         // an extra copy not to really matter.
@@ -1249,8 +1263,16 @@ bool wxBMPHandler::LoadDib(wxImage *image, wxInputStream& stream,
         }
     }
 
+    // We may have a gap between the palette and start of the pixel data, skip
+    // it if necessary.
+    if ( bytesRead < offset )
+    {
+        if ( stream.SeekI(offset - bytesRead, wxFromCurrent) == wxInvalidOffset )
+            return false;
+    }
+
     //read DIB; this is the BMP image or the XOR part of an icon image
-    if ( !LoadBMPData(image, desc, offset, stream, verbose, IsBmp) )
+    if ( !LoadBMPData(image, desc, stream, verbose) )
     {
         if (verbose)
         {
@@ -1271,7 +1293,7 @@ bool wxBMPHandler::LoadDib(wxImage *image, wxInputStream& stream,
 
         //there is no palette, so we will create one
         wxImage mask;
-        if ( !LoadBMPData(&mask, descMask, offset, stream, verbose, IsBmp) )
+        if ( !LoadBMPData(&mask, descMask, stream, verbose) )
         {
             if (verbose)
             {

build/cmake/tests/gui/CMakeLists.txt
@@ -145,6 +145,7 @@ set(TEST_GUI_DATA
     horse.tga
     horse.tif
     horse.xpm
+    image/bitfields.bmp
     image/8bpp-colorsused-large.bmp
     image/8bpp-colorsused-negative.bmp
     image/rle4-delta-320x240.bmp

tests/Makefile.in
@@ -577,7 +577,7 @@ data-image-sample:
 
 data-images: 
 	@mkdir -p image
-	@for f in 8bpp-colorsused-large.bmp 8bpp-colorsused-negative.bmp rle4-delta-320x240.bmp rle8-delta-320x240.bmp rle8-delta-320x240-expected.bmp horse_grey.bmp horse_grey_flipped.bmp horse_rle4.bmp horse_rle4_flipped.bmp horse_rle8.bmp horse_rle8_flipped.bmp horse_bicubic_50x50.png horse_bicubic_100x100.png horse_bicubic_150x150.png horse_bicubic_300x300.png horse_bilinear_50x50.png horse_bilinear_100x100.png horse_bilinear_150x150.png horse_bilinear_300x300.png horse_box_average_50x50.png horse_box_average_100x100.png horse_box_average_150x150.png horse_box_average_300x300.png cross_bicubic_256x256.png cross_bilinear_256x256.png cross_box_average_256x256.png cross_nearest_neighb_256x256.png paste_input_background.png paste_input_black.png paste_input_overlay_transparent_border_opaque_square.png paste_input_overlay_transparent_border_semitransparent_circle.png paste_input_overlay_transparent_border_semitransparent_square.png paste_result_background_plus_circle_plus_square.png paste_result_background_plus_overlay_transparent_border_opaque_square.png paste_result_background_plus_overlay_transparent_border_semitransparent_square.png paste_result_no_background_square_over_circle.png wx.png wx.ico toucan.png toucan_hue_0.538.png toucan_sat_-0.41.png toucan_bright_-0.259.png toucan_hsv_0.538_-0.41_-0.259.png toucan_light_46.png toucan_dis_240.png toucan_grey.png toucan_mono_255_255_255.png width-times-height-overflow.bmp width_height_32_bit_overflow.pgm; do \
+	@for f in bitfields.bmp 8bpp-colorsused-large.bmp 8bpp-colorsused-negative.bmp rle4-delta-320x240.bmp rle8-delta-320x240.bmp rle8-delta-320x240-expected.bmp horse_grey.bmp horse_grey_flipped.bmp horse_rle4.bmp horse_rle4_flipped.bmp horse_rle8.bmp horse_rle8_flipped.bmp horse_bicubic_50x50.png horse_bicubic_100x100.png horse_bicubic_150x150.png horse_bicubic_300x300.png horse_bilinear_50x50.png horse_bilinear_100x100.png horse_bilinear_150x150.png horse_bilinear_300x300.png horse_box_average_50x50.png horse_box_average_100x100.png horse_box_average_150x150.png horse_box_average_300x300.png cross_bicubic_256x256.png cross_bilinear_256x256.png cross_box_average_256x256.png cross_nearest_neighb_256x256.png paste_input_background.png paste_input_black.png paste_input_overlay_transparent_border_opaque_square.png paste_input_overlay_transparent_border_semitransparent_circle.png paste_input_overlay_transparent_border_semitransparent_square.png paste_result_background_plus_circle_plus_square.png paste_result_background_plus_overlay_transparent_border_opaque_square.png paste_result_background_plus_overlay_transparent_border_semitransparent_square.png paste_result_no_background_square_over_circle.png wx.png wx.ico toucan.png toucan_hue_0.538.png toucan_sat_-0.41.png toucan_bright_-0.259.png toucan_hsv_0.538_-0.41_-0.259.png toucan_light_46.png toucan_dis_240.png toucan_grey.png toucan_mono_255_255_255.png width-times-height-overflow.bmp width_height_32_bit_overflow.pgm; do \
 	if test ! -f image/$$f -a ! -d image/$$f ; \
 	then x=yep ; \
 	else x=`find $(srcdir)/image/$$f -newer image/$$f -print` ; \

tests/image/image.cpp
@@ -164,6 +164,8 @@ void ImageTestCase::LoadFromFile()
         INFO("Loading " << file);
         CHECK(img.LoadFile(file));
     }
+
+    CHECK(img.LoadFile("image/bitfields.bmp", wxBITMAP_TYPE_BMP));
 }
 
 void ImageTestCase::LoadFromSocketStream()

tests/makefile.gcc
@@ -559,7 +559,7 @@ data-image-sample:
 
 data-images: 
 	if not exist image mkdir image
-	for %%f in (8bpp-colorsused-large.bmp 8bpp-colorsused-negative.bmp rle4-delta-320x240.bmp rle8-delta-320x240.bmp rle8-delta-320x240-expected.bmp horse_grey.bmp horse_grey_flipped.bmp horse_rle4.bmp horse_rle4_flipped.bmp horse_rle8.bmp horse_rle8_flipped.bmp horse_bicubic_50x50.png horse_bicubic_100x100.png horse_bicubic_150x150.png horse_bicubic_300x300.png horse_bilinear_50x50.png horse_bilinear_100x100.png horse_bilinear_150x150.png horse_bilinear_300x300.png horse_box_average_50x50.png horse_box_average_100x100.png horse_box_average_150x150.png horse_box_average_300x300.png cross_bicubic_256x256.png cross_bilinear_256x256.png cross_box_average_256x256.png cross_nearest_neighb_256x256.png paste_input_background.png paste_input_black.png paste_input_overlay_transparent_border_opaque_square.png paste_input_overlay_transparent_border_semitransparent_circle.png paste_input_overlay_transparent_border_semitransparent_square.png paste_result_background_plus_circle_plus_square.png paste_result_background_plus_overlay_transparent_border_opaque_square.png paste_result_background_plus_overlay_transparent_border_semitransparent_square.png paste_result_no_background_square_over_circle.png wx.png wx.ico toucan.png toucan_hue_0.538.png toucan_sat_-0.41.png toucan_bright_-0.259.png toucan_hsv_0.538_-0.41_-0.259.png toucan_light_46.png toucan_dis_240.png toucan_grey.png toucan_mono_255_255_255.png width-times-height-overflow.bmp width_height_32_bit_overflow.pgm) do if not exist image\%%f copy .\image\%%f image
+	for %%f in (bitfields.bmp 8bpp-colorsused-large.bmp 8bpp-colorsused-negative.bmp rle4-delta-320x240.bmp rle8-delta-320x240.bmp rle8-delta-320x240-expected.bmp horse_grey.bmp horse_grey_flipped.bmp horse_rle4.bmp horse_rle4_flipped.bmp horse_rle8.bmp horse_rle8_flipped.bmp horse_bicubic_50x50.png horse_bicubic_100x100.png horse_bicubic_150x150.png horse_bicubic_300x300.png horse_bilinear_50x50.png horse_bilinear_100x100.png horse_bilinear_150x150.png horse_bilinear_300x300.png horse_box_average_50x50.png horse_box_average_100x100.png horse_box_average_150x150.png horse_box_average_300x300.png cross_bicubic_256x256.png cross_bilinear_256x256.png cross_box_average_256x256.png cross_nearest_neighb_256x256.png paste_input_background.png paste_input_black.png paste_input_overlay_transparent_border_opaque_square.png paste_input_overlay_transparent_border_semitransparent_circle.png paste_input_overlay_transparent_border_semitransparent_square.png paste_result_background_plus_circle_plus_square.png paste_result_background_plus_overlay_transparent_border_opaque_square.png paste_result_background_plus_overlay_transparent_border_semitransparent_square.png paste_result_no_background_square_over_circle.png wx.png wx.ico toucan.png toucan_hue_0.538.png toucan_sat_-0.41.png toucan_bright_-0.259.png toucan_hsv_0.538_-0.41_-0.259.png toucan_light_46.png toucan_dis_240.png toucan_grey.png toucan_mono_255_255_255.png width-times-height-overflow.bmp width_height_32_bit_overflow.pgm) do if not exist image\%%f copy .\image\%%f image
 
 fr: 
 	if not exist $(OBJS)\intl\fr mkdir $(OBJS)\intl\fr

tests/makefile.vc
@@ -857,7 +857,7 @@ data-image-sample:
 
 data-images: 
 	if not exist image mkdir image
-	for %f in (8bpp-colorsused-large.bmp 8bpp-colorsused-negative.bmp rle4-delta-320x240.bmp rle8-delta-320x240.bmp rle8-delta-320x240-expected.bmp horse_grey.bmp horse_grey_flipped.bmp horse_rle4.bmp horse_rle4_flipped.bmp horse_rle8.bmp horse_rle8_flipped.bmp horse_bicubic_50x50.png horse_bicubic_100x100.png horse_bicubic_150x150.png horse_bicubic_300x300.png horse_bilinear_50x50.png horse_bilinear_100x100.png horse_bilinear_150x150.png horse_bilinear_300x300.png horse_box_average_50x50.png horse_box_average_100x100.png horse_box_average_150x150.png horse_box_average_300x300.png cross_bicubic_256x256.png cross_bilinear_256x256.png cross_box_average_256x256.png cross_nearest_neighb_256x256.png paste_input_background.png paste_input_black.png paste_input_overlay_transparent_border_opaque_square.png paste_input_overlay_transparent_border_semitransparent_circle.png paste_input_overlay_transparent_border_semitransparent_square.png paste_result_background_plus_circle_plus_square.png paste_result_background_plus_overlay_transparent_border_opaque_square.png paste_result_background_plus_overlay_transparent_border_semitransparent_square.png paste_result_no_background_square_over_circle.png wx.png wx.ico toucan.png toucan_hue_0.538.png toucan_sat_-0.41.png toucan_bright_-0.259.png toucan_hsv_0.538_-0.41_-0.259.png toucan_light_46.png toucan_dis_240.png toucan_grey.png toucan_mono_255_255_255.png width-times-height-overflow.bmp width_height_32_bit_overflow.pgm) do if not exist image\%f copy .\image\%f image
+	for %f in (bitfields.bmp 8bpp-colorsused-large.bmp 8bpp-colorsused-negative.bmp rle4-delta-320x240.bmp rle8-delta-320x240.bmp rle8-delta-320x240-expected.bmp horse_grey.bmp horse_grey_flipped.bmp horse_rle4.bmp horse_rle4_flipped.bmp horse_rle8.bmp horse_rle8_flipped.bmp horse_bicubic_50x50.png horse_bicubic_100x100.png horse_bicubic_150x150.png horse_bicubic_300x300.png horse_bilinear_50x50.png horse_bilinear_100x100.png horse_bilinear_150x150.png horse_bilinear_300x300.png horse_box_average_50x50.png horse_box_average_100x100.png horse_box_average_150x150.png horse_box_average_300x300.png cross_bicubic_256x256.png cross_bilinear_256x256.png cross_box_average_256x256.png cross_nearest_neighb_256x256.png paste_input_background.png paste_input_black.png paste_input_overlay_transparent_border_opaque_square.png paste_input_overlay_transparent_border_semitransparent_circle.png paste_input_overlay_transparent_border_semitransparent_square.png paste_result_background_plus_circle_plus_square.png paste_result_background_plus_overlay_transparent_border_opaque_square.png paste_result_background_plus_overlay_transparent_border_semitransparent_square.png paste_result_no_background_square_over_circle.png wx.png wx.ico toucan.png toucan_hue_0.538.png toucan_sat_-0.41.png toucan_bright_-0.259.png toucan_hsv_0.538_-0.41_-0.259.png toucan_light_46.png toucan_dis_240.png toucan_grey.png toucan_mono_255_255_255.png width-times-height-overflow.bmp width_height_32_bit_overflow.pgm) do if not exist image\%f copy .\image\%f image
 
 fr: 
 	if not exist $(OBJS)\intl\fr mkdir $(OBJS)\intl\fr

tests/test.bkl
@@ -351,6 +351,7 @@
         <srcdir>$(SRCDIR)/image</srcdir>
         <dstdir>image</dstdir>
         <files>
+            bitfields.bmp
             8bpp-colorsused-large.bmp
             8bpp-colorsused-negative.bmp
             rle4-delta-320x240.bmp
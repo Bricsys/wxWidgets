Bricsys/wxWidgets

commit 7061bd1b9786388612ba7b3529db9b13f8d86991

Fix memory leak of command line arguments in wxMSW
Use the same hack already used for wxEntryReal() before to define a
MSW-specific wxEntryCleanup() which frees the memory allocated by
::CommandLineToArgvW() after performing the common cleanup.

Also refactor the code dealing with them to avoid having different
wxArgs variables in wxBase and wxCode, which was almost certainly
unintentional and definitely very confusing. Now we keep just a single
wxArgs in wxBase and provide access to it from wxCore via the exported
functions.

See #23081, #23082.

(cherry picked from commit 3edc2982890246797295b10be1ec621ff96e64f0)

src/msw/main.cpp
@@ -42,7 +42,7 @@
 
 // defined in common/init.cpp
 extern int wxEntryReal(int& argc, wxChar **argv);
-extern int wxEntryCleanupReal(int& argc, wxChar **argv);
+extern void wxEntryCleanupReal();
 
 // ============================================================================
 // implementation: various entry points
@@ -192,6 +192,18 @@ int wxEntry(int& argc, wxChar **argv)
 // Windows-specific wxEntry
 // ----------------------------------------------------------------------------
 
+// Declare the functions used in wxCore to access the command line arguments
+// data in wxBase.
+WXDLLIMPEXP_BASE void wxMSWCommandLineInit();
+WXDLLIMPEXP_BASE void wxMSWCommandLineCleanup();
+WXDLLIMPEXP_BASE int& wxMSWCommandLineGetArgc();
+WXDLLIMPEXP_BASE wchar_t** wxMSWCommandLineGetArgv();
+
+#if wxUSE_BASE
+
+namespace
+{
+
 struct wxMSWCommandLineArguments
 {
     wxMSWCommandLineArguments() { argc = 0; argv = NULL; }
@@ -207,10 +219,18 @@ struct wxMSWCommandLineArguments
         argv = ::CommandLineToArgvW(::GetCommandLineW(), &argc);
     }
 
-    ~wxMSWCommandLineArguments()
+    void Cleanup()
     {
         if ( argc )
+        {
             ::LocalFree(argv);
+            argc = 0;
+        }
+    }
+
+    ~wxMSWCommandLineArguments()
+    {
+        Cleanup();
     }
 #else // !wxUSE_UNICODE
     void Init()
@@ -253,9 +273,35 @@ struct wxMSWCommandLineArguments
 
     int argc;
     wxChar **argv;
+
+    wxDECLARE_NO_COPY_CLASS(wxMSWCommandLineArguments);
 };
 
 static wxMSWCommandLineArguments wxArgs;
+
+} // anonymous namespace
+
+WXDLLIMPEXP_BASE void wxMSWCommandLineInit()
+{
+    wxArgs.Init();
+}
+
+WXDLLIMPEXP_BASE void wxMSWCommandLineCleanup()
+{
+    wxArgs.Cleanup();
+}
+
+WXDLLIMPEXP_BASE int& wxMSWCommandLineGetArgc()
+{
+    return wxArgs.argc;
+}
+
+WXDLLIMPEXP_BASE wchar_t** wxMSWCommandLineGetArgv()
+{
+    return wxArgs.argv;
+}
+
+#endif // wxUSE_BASE
 
 #if wxUSE_GUI
 
@@ -271,7 +317,7 @@ wxMSWEntryCommon(HINSTANCE hInstance, int nCmdShow)
     wxUnusedVar(nCmdShow);
 #endif
 
-    wxArgs.Init();
+    wxMSWCommandLineInit();
 
     return true;
 }
@@ -284,7 +330,7 @@ WXDLLEXPORT bool wxEntryStart(HINSTANCE hInstance,
     if ( !wxMSWEntryCommon(hInstance, nCmdShow) )
        return false;
 
-    return wxEntryStart(wxArgs.argc, wxArgs.argv);
+    return wxEntryStart(wxMSWCommandLineGetArgc(), wxMSWCommandLineGetArgv());
 }
 
 WXDLLEXPORT int wxEntry(HINSTANCE hInstance,
@@ -295,7 +341,7 @@ WXDLLEXPORT int wxEntry(HINSTANCE hInstance,
     if ( !wxMSWEntryCommon(hInstance, nCmdShow) )
         return -1;
 
-    return wxEntry(wxArgs.argc, wxArgs.argv);
+    return wxEntry(wxMSWCommandLineGetArgc(), wxMSWCommandLineGetArgv());
 }
 
 #endif // wxUSE_GUI
@@ -308,9 +354,16 @@ WXDLLEXPORT int wxEntry(HINSTANCE hInstance,
 
 int wxEntry()
 {
-    wxArgs.Init();
+    wxMSWCommandLineInit();
 
-    return wxEntry(wxArgs.argc, wxArgs.argv);
+    return wxEntry(wxMSWCommandLineGetArgc(), wxMSWCommandLineGetArgv());
+}
+
+void wxEntryCleanup()
+{
+    wxEntryCleanupReal();
+
+    wxMSWCommandLineCleanup();
 }
 
 HINSTANCE wxhInstance = 0;

src/common/init.cpp
@@ -436,7 +436,12 @@ static void DoCommonPostCleanup()
 #endif // wxUSE_LOG
 }
 
-void wxEntryCleanup()
+// for MSW the real wxEntryCleanup() is defined in msw/main.cpp
+#ifndef __WINDOWS__
+    #define wxEntryCleanupReal wxEntryCleanup
+#endif // !__WINDOWS__
+
+void wxEntryCleanupReal()
 {
     DoCommonPreCleanup();
 

commit df7971dadd13edcb7c88d91c1bfd133ccf87712f

Fix non-Unicode wxMSW build after command line arguments fixes
The changes of 7061bd1b97 (Fix memory leak of command line arguments in
wxMSW, 2022-12-26) broke compilation with wxUSE_UNICODE==0 as they were
backported from master where this case isn't possible any more.

Adjust these changes to work in non-Unicode build too.

See #23082, #23516.

src/msw/main.cpp
@@ -197,7 +197,7 @@ int wxEntry(int& argc, wxChar **argv)
 WXDLLIMPEXP_BASE void wxMSWCommandLineInit();
 WXDLLIMPEXP_BASE void wxMSWCommandLineCleanup();
 WXDLLIMPEXP_BASE int& wxMSWCommandLineGetArgc();
-WXDLLIMPEXP_BASE wchar_t** wxMSWCommandLineGetArgv();
+WXDLLIMPEXP_BASE wxChar** wxMSWCommandLineGetArgv();
 
 #if wxUSE_BASE
 
@@ -227,11 +227,6 @@ struct wxMSWCommandLineArguments
             argc = 0;
         }
     }
-
-    ~wxMSWCommandLineArguments()
-    {
-        Cleanup();
-    }
 #else // !wxUSE_UNICODE
     void Init()
     {
@@ -256,7 +251,7 @@ struct wxMSWCommandLineArguments
         argv[argc] = NULL;
     }
 
-    ~wxMSWCommandLineArguments()
+    void Cleanup()
     {
         if ( !argc )
             return;
@@ -270,6 +265,11 @@ struct wxMSWCommandLineArguments
         argc = 0;
     }
 #endif // wxUSE_UNICODE/!wxUSE_UNICODE
+
+    ~wxMSWCommandLineArguments()
+    {
+        Cleanup();
+    }
 
     int argc;
     wxChar **argv;
@@ -296,7 +296,7 @@ WXDLLIMPEXP_BASE int& wxMSWCommandLineGetArgc()
     return wxArgs.argc;
 }
 
-WXDLLIMPEXP_BASE wchar_t** wxMSWCommandLineGetArgv()
+WXDLLIMPEXP_BASE wxChar** wxMSWCommandLineGetArgv()
 {
     return wxArgs.argv;
 }
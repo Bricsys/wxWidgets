Bricsys/wxWidgets

commit 72d35b792d8c577010d251c8b8a7c732b5dcece9

Don't show bogus value when there are none in wxGTK wxDVC neither
This is similar to the previous change to the generic version and simply
applies the same logic to the cells for which GetValue() returns null
value as for those for which HasValue() returns false.

src/gtk/dataview.cpp
@@ -3171,6 +3171,14 @@ gtk_dataview_header_button_press_callback( GtkWidget *WXUNUSED(widget),
     return FALSE;
 }
 
+// Helper for wxGtkTreeCellDataFunc() below.
+static void wxGtkTreeSetVisibleProp(GtkCellRenderer *renderer, gboolean visible)
+{
+    wxGtkValue gvalue( G_TYPE_BOOLEAN );
+    g_value_set_boolean( gvalue, visible );
+    g_object_set_property( G_OBJECT(renderer), "visible", gvalue );
+}
+
 extern "C"
 {
 
@@ -3200,16 +3208,22 @@ static void wxGtkTreeCellDataFunc( GtkTreeViewColumn *WXUNUSED(column),
     if (!wx_model->IsVirtualListModel())
     {
         gboolean visible = wx_model->HasValue(item, column);
-        wxGtkValue gvalue( G_TYPE_BOOLEAN );
-        g_value_set_boolean( gvalue, visible );
-        g_object_set_property( G_OBJECT(renderer), "visible", gvalue );
+        wxGtkTreeSetVisibleProp(renderer, visible);
 
         if ( !visible )
             return;
     }
 
     cell->GtkSetCurrentItem(item);
-    cell->PrepareForItem(wx_model, item, column);
+
+    if (!cell->PrepareForItem(wx_model, item, column))
+    {
+        // We don't have any value in this cell, after all, so hide it.
+        if (!wx_model->IsVirtualListModel())
+        {
+            wxGtkTreeSetVisibleProp(renderer, FALSE);
+        }
+    }
 }
 
 } // extern "C"

commit ae7a4b1d5ec78377a8da461634c27a3153bb3bf4

Fix editing cells without values in wxGTK wxDataViewCtrl
Do not hide cells without values at GTK level, as this makes them not
only appear as blank (which is fine), but also prevents them from being
edited, which is not, as the user should be able to enter values into
the previously empty cells.

This fixes regression introduced back in 72d35b792d (Don't show bogus
value when there are none in wxGTK wxDVC neither, 2021-12-01).

Note that this also undoes the changes of 702503976f (Remove duplicated
HasValue() call from wxGTK wxDataViewCtrl code, 2022-05-08) and
HasValue() is called twice again now, but this is less of a problem than
wrong behaviour fixed by this commit and we can address it later, e.g.
by passing the cell value to PrepareForItem() instead of retrieving it
inside it, if it turns out to be really worth it.

See #23523.

(cherry picked from commit 821c2a07fe2b29c283e11f25b184e3d6d9a17013)

src/gtk/dataview.cpp
@@ -3224,10 +3224,18 @@ static void wxGtkTreeCellDataFunc( GtkTreeViewColumn *WXUNUSED(column),
 
     wxDataViewModel *wx_model = tree_model->internal->GetDataViewModel();
 
-    cell->GtkSetCurrentItem(item);
-
     // Cells without values shouldn't be rendered at all.
-    const bool visible = cell->PrepareForItem(wx_model, item, column);
+    const bool visible = wx_model->HasValue(item, column);
+    if ( visible )
+    {
+        cell->GtkSetCurrentItem(item);
+
+        // Ignore the return value of PrepareForItem() here, if it returns
+        // false because GetValue() didn't return anything, we still want to
+        // keep this cell visible, as otherwise it wouldn't be possible to edit
+        // it neither, and we do want to allow editing empty cells.
+        cell->PrepareForItem(wx_model, item, column);
+    }
 
     wxGtkValue gvalue( G_TYPE_BOOLEAN );
     g_value_set_boolean( gvalue, visible );
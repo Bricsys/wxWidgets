Bricsys/wxWidgets

commit 73db033aea2d69293a2b1e1fb75629782eb72ed5

Fix position of label window in wxStaticBox after DPI change
See #23740.

(cherry picked from commit d847f3b039f469000e4a0673ae1f88a81e889248)

src/msw/statbox.cpp
@@ -98,6 +98,8 @@ bool wxStaticBox::Create(wxWindow *parent,
         SetBackgroundStyle(wxBG_STYLE_PAINT);
     }
 
+    Bind(wxEVT_DPI_CHANGED, &wxStaticBox::OnDPIChanged, this);
+
     return true;
 }
 
@@ -304,6 +306,11 @@ WXLRESULT wxStaticBox::MSWWindowProc(WXUINT nMsg, WXWPARAM wParam, WXLPARAM lPar
     }
 
     return wxControl::MSWWindowProc(nMsg, wParam, lParam);
+}
+
+void wxStaticBox::OnDPIChanged(wxDPIChangedEvent& WXUNUSED(event))
+{
+    PositionLabelWindow();
 }
 
 // ----------------------------------------------------------------------------

include/wx/msw/statbox.h
@@ -93,6 +93,8 @@ protected:
     void OnPaint(wxPaintEvent& event);
 
 private:
+    void OnDPIChanged(wxDPIChangedEvent& event);
+
     void PositionLabelWindow();
 
     wxDECLARE_DYNAMIC_CLASS_NO_COPY(wxStaticBox);

commit d671d754480dfcb7e47acf1816389fc3befa6754

Fix crash on DPI change for wxStaticBox without a control label
Check that we have a label before repositioning it fix the crash
introduced by 73db033aea (Fix position of label window in wxStaticBox
after DPI change, 2023-08-05).

See #23740, #23826.

(cherry picked from commit 329d35b922aa893c292d14061fb099aacb4af20b)

src/msw/statbox.cpp
@@ -310,7 +310,8 @@ WXLRESULT wxStaticBox::MSWWindowProc(WXUINT nMsg, WXWPARAM wParam, WXLPARAM lPar
 
 void wxStaticBox::OnDPIChanged(wxDPIChangedEvent& WXUNUSED(event))
 {
-    PositionLabelWindow();
+    if ( m_labelWin )
+        PositionLabelWindow();
 }
 
 // ----------------------------------------------------------------------------
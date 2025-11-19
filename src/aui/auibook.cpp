401d6400bace1e3ebe53a2e0ba53232f78fbcc57
iolandaasavei <iolanda.asavei@hexagon.com>
2025-11-19 23:59:09 +0200

    [api-change] Allow wxAuiNotebook to use custom tab-control classes
    MERGED REVISIONS: r1755 [r2546]
    AUTHORS: svn-owenw (Owen Wengerd)
    
    Added support for using a derived wxAuiTabCtrl in wxAuiNotebook so custom tab controls can be integrated into the AUI notebook.
    
    Co-authored-by: Owen Wengerd <owen.wengerd@hexagon.com>

--- src/aui/auibook.cpp
+++ src/aui/auibook.cpp
@@ -2327,7 +2327,8 @@ wxAuiTabCtrl* wxAuiNotebook::GetActiveTabCtrl()
     // If there is no tabframe at all, create one
     wxTabFrame* tabframe = new wxTabFrame;
     tabframe->SetTabCtrlHeight(m_tabCtrlHeight);
-    tabframe->m_tabs = new wxAuiTabCtrl(this,
+    // Bricsys change: enable replacement of stock wxAuiTabCtrl with derived class
+    tabframe->m_tabs = createTabControl(this, 
                                         m_tabIdCounter++,
                                         wxDefaultPosition,
                                         wxDefaultSize,
@@ -2410,7 +2411,8 @@ void wxAuiNotebook::Split(size_t page, int direction)
     wxTabFrame* new_tabs = new wxTabFrame;
     new_tabs->m_rect = wxRect(wxPoint(0,0), split_size);
     new_tabs->SetTabCtrlHeight(m_tabCtrlHeight);
-    new_tabs->m_tabs = new wxAuiTabCtrl(this,
+    // Bricsys change: enable replacement of stock wxAuiTabCtrl with derived class
+    new_tabs->m_tabs = createTabControl(this, 
                                         m_tabIdCounter++,
                                         wxDefaultPosition,
                                         wxDefaultSize,
@@ -2826,7 +2828,8 @@ void wxAuiNotebook::OnTabEndDrag(wxAuiNotebookEvent& evt)
             wxTabFrame* new_tabs = new wxTabFrame;
             new_tabs->m_rect = wxRect(wxPoint(0,0), CalculateNewSplitSize());
             new_tabs->SetTabCtrlHeight(m_tabCtrlHeight);
-            new_tabs->m_tabs = new wxAuiTabCtrl(this,
+            // Bricsys change: enable replacement of stock wxAuiTabCtrl with derived class
+            new_tabs->m_tabs = createTabControl(this, 
                                                 m_tabIdCounter++,
                                                 wxDefaultPosition,
                                                 wxDefaultSize,
@@ -3628,6 +3631,13 @@ int wxAuiNotebook::DoModifySelection(size_t n, bool events)
     }
 
     return m_curPage;
+}
+
+// Bricsys change: enable replacement of stock wxAuiTabCtrl with derived class
+wxAuiTabCtrl* wxAuiNotebook::createTabControl(wxWindow* parent, wxWindowID id,
+                                          const wxPoint& pos, const wxSize& size, long style)
+{
+    return new wxAuiTabCtrl(parent, id, pos, size, style);
 }
 
 void wxAuiTabCtrl::SetHoverTab(wxWindow* wnd)

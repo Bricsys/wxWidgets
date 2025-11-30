79543cc5f415a4a687f6b92efbd3c97abb5e88bd
iolandaasavei <iolanda.asavei@hexagon.com>
2025-11-30 13:39:58 +0200

    [api-change] Improved mac touchpad rotation, pinch, and pan detection
    MERGED REVISIONS: r1895 r1900 r1923 r1924 r1925 r2029
    AUTHORS: svn-razvanm (Razvan Macovei)
    MERGED REVISIONS: [r2579] [r2581] [r2583] [r2611]
    AUTHORS: svn-razvanm (Razvan Macovei)
    
    - fixed incorrect detection of rotation, pinch, and pan
    - used gesture events for rotation/pinch and touch/scroll events for pan
    - smoothed pinch-zoom behavior and removed accidental double-tap zooms
    - reset gesture state when the application becomes inactive
    - aligned event processing with wxWidgets Cocoa handling and removed redundant panning-end checks
    - fixed unwanted zoom-out caused by weak conditions in mouseEvent
    - improved pan reliability by using gesture scroll phases
    - differentiated trackpad vs Magic Mouse behavior using isTouch and momentum vs non-momentum scroll events
    
    Co-authored-by: Razvan Macovei <razvan.macovei@hexagon.com>
    refs RM-5766

--- src/osx/cocoa/trackerTouchDouble.mm
+++ src/osx/cocoa/trackerTouchDouble.mm
@@ -3,7 +3,6 @@
 
 @interface DualTouchTracker()
   @property BOOL isTracking;
-  @property BOOL dualTapDownWasSent;
   @property(readwrite) NSPoint initialPoint;
   @property(readwrite) NSUInteger modifiers;
   - (void)releaseTouches;
@@ -21,7 +20,6 @@
         self.threshold = 1;
         self.pannThreshold = 0.0014;
         self.oldDeltaSizeTouches = 0;
-        self.dualTapDownWasSent = FALSE;
     }
     
     return self;
@@ -52,9 +50,7 @@
         _oldTouches[0] = [ _currentTouches[0] retain];
         _oldTouches[1] = [ _currentTouches[1] retain];
 
-        _currentEvent = [event retain];
-
-        [self sendDualTapDown];
+        _currentTouchEvent = [event retain];
 
     } else if (touches.count > 2) {
         // More than 2 touches. Only track 2.
@@ -84,7 +80,7 @@
 
         [_currentTouches[0] release];
         [_currentTouches[1] release];
-        [_currentEvent release];
+        [_currentTouchEvent release];
         
         NSTouch *touch;
         touch = [array objectAtIndex:0];
@@ -100,7 +96,7 @@
         } else {
             _currentTouches[1] = [touch retain];
         }
-        _currentEvent = [event retain];
+        _currentTouchEvent = [event retain];
         
         if (!self.isTracking)
         {
@@ -123,12 +119,10 @@
     if (!self.isEnabled) return;
     
     self.modifiers = [event modifierFlags];
-    [self sendDualTapUp];
     [self cancelTracking];
 }
 
 - (void)touchesCancelledWithEvent:(NSEvent *)event {
-    [self sendDualTapUp];
     [self cancelTracking];
 }
 
@@ -141,17 +135,30 @@
     }
 }
 
-- (void)sendDualTapDown {
-    if (self.dualTapDownAction) {
-        [NSApp sendAction:self.dualTapDownAction to:self.view from:self];
-        self.dualTapDownWasSent = TRUE;
+- (void)magnifyWithEvent:(NSEvent *)event
+{
+    if (self.magnifyAction)
+    {
+        _currentGestureEvent = [event retain];
+        [NSApp sendAction:self.magnifyAction to:self.view from:self];
     }
 }
 
-- (void)sendDualTapUp {
-    if (self.dualTapDownWasSent) {
-        if (self.dualTapUpAction) [NSApp sendAction:self.dualTapUpAction to:self.view from:self];
-        self.dualTapDownWasSent = FALSE;
+- (void)rotateWithEvent:(NSEvent *)event
+{
+    if (self.rotateAction)
+    {
+        _currentGestureEvent = [event retain];
+        [NSApp sendAction:self.rotateAction to:self.view from:self];
+    }
+}
+
+- (void)smartMagnifyWithEvent:(NSEvent *)event
+{
+    if (self.doubleTapAction)
+    {
+        _currentGestureEvent = [event retain];
+        [NSApp sendAction:self.doubleTapAction to:self.view from:self];
     }
 }
 
@@ -166,9 +173,16 @@
 @synthesize beginTrackingAction = _beginTrackingAction;
 @synthesize updateTrackingAction = _updateTrackingAction;
 @synthesize endTrackingAction = _endTrackingAction;
+@synthesize rotateAction = _rotateAction;
+@synthesize magnifyAction = _magnifyAction;
+@synthesize doubleTapAction = _doubleTapAction;
 
-- (NSEvent *) getCurrentEvent{
-    return _currentEvent;
+- (NSEvent *) getCurrentTouchEvent{
+    return _currentTouchEvent;
+}
+
+- (NSEvent *) getCurrentGestureEvent{
+    return _currentGestureEvent;
 }
 
 - (NSPoint)deltaOrigin {
@@ -412,8 +426,8 @@
 }
 
 - (void)releaseEvent {
-    [_currentEvent release];
-    _currentEvent = nil;
+    [_currentTouchEvent release];
+    _currentTouchEvent = nil;
 }
 
 @end // end DualTouchTracker implementation
